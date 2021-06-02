b7908a017cd917d7172a32dae81d778065e01693
gcapurso <giovanna.capurso@gmail.com>
2021-06-02 13:43:00 +0200

    Update models

--- app/models/administrative-unit.js
+++ app/models/administrative-unit.js
@@ -1,5 +1,12 @@
-import Model from '@ember-data/model';
+import Model, { attr, hasMany, belongsTo } from '@ember-data/model';
 
 export default class AdministrativeUnitModel extends Model {
-
+    @attr name;
+    @attr alternativeName;
+    @belongsTo('administrative-unit-classification-code') classification;
+    @belongsTo('site') primarySite;
+    @hasMany('identifier', { inverse: null }) identifiers;
+    @hasMany('governing-body', { inverse: null }) governingBodies;
+    @hasMany('contact-point', { inverse: null }) contactPoints;
+    @hasMany('site', { inverse: null }) sites;
 }
--- app/models/governing-body.js
+++ app/models/governing-body.js
@@ -1,5 +1,11 @@
-import Model from '@ember-data/model';
+import Model, { attr, hasMany, belongsTo } from '@ember-data/model';
 
 export default class GoverningBodyModel extends Model {
-
+    @attr startDate;
+    @attr endDate;
+    @belongsTo('administrative-unit') administrativeUnit;
+    @belongsTo('governing-body-classification-code') classification;
+    @belongsTo('governing-body') isTimeSpecializationOf;
+    @hasMany('governing-body', { inverse: null }) hasTimeSpecializations;
+    @hasMany('mandate', { inverse: null }) contains;
 }
