7b84d16476ce3c9a9b5209f913550c412239be1e
anitacaron <anitarcaron@gmail.com>
2021-06-03 20:45:42 +0100

    Fetch data for Administrative Unit

--- app/controllers/people/index.js
+++ app/controllers/people/index.js
@@ -1,10 +1,28 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class PeopleIndexController extends Controller {
-  @service router;
+  @tracked shortedModel;
 
-  page = 0;
-  size = 20;
+  constructor() {
+    super(...arguments);
+    this.getAdminUnitInfo()
+  }
+
+  async getAdminUnitInfo() {
+    //let shortedModel = [];
+
+    const mandatories = await this.model.mandatories;
+    const mandate = await mandatories[0].mandate;
+    const tempGoverningBody = await mandate.governingBody;
+    const governingBody = await tempGoverningBody.isTimeSpecializationOf;
+    const adminUnit = await governingBody.administrativeUnit;
+    const role = await mandate.roleBoard;
+
+    // TODO: Array to fill the table. Include only:  this.model.givenName, this.model.familyName, adminUnit.name and role.label
+        
+    //this.shortedModel = shortedModel
+    
+  }
 
 }
--- app/models/mandate.js
+++ app/models/mandate.js
@@ -2,6 +2,6 @@ import Model, {hasMany, belongsTo } from '@ember-data/model';
 
 export default class MandateModel extends Model {
     @belongsTo('board-position') roleBoard;
-    @hasMany('governing-body', { inverse: true }) governingBodies;
+    @belongsTo('governing-body', { inverse: true }) governingBody;
     @hasMany('mandatory', { inverse: true }) heldBy;
 }
