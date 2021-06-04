e6366b93d9b7508f97e54724110a32f880454d0c
anitacaron <anitarcaron@gmail.com>
2021-06-04 17:17:16 +0100

    Get eredienst types

--- app/controllers/administrative-units/index.js
+++ app/controllers/administrative-units/index.js
@@ -1,5 +1,40 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class AdministrativeUnitsIndexController extends Controller {
+  @service store;
 
+  @tracked typeEredienst = null;
+  @tracked typesEredienstOptions;
+
+  @tracked stausOrg = null;
+  
+  constructor() {
+    super(...arguments);
+    this.getTypesEredienst()
+  }
+
+  async getTypesEredienst() {
+    const typesEredienstOptions = await this.store.findAll('honorary-service-type');
+
+    this.typesEredienstOptions = typesEredienstOptions;
+    
+  }
+  
+  @action
+  async handleTypeEredienstSelection(typeEredienst) {
+    if (typeEredienst) {
+      this.typeEredienst = typeEredienst;
+      
+      this.model.filter((adminUnit) => {
+        return (
+          adminUnit.honoraryServiceType.label == typeEredienst
+        )
+      });
+    } else {
+      this.typeEredienst = null;
+    }
+  }
 }
