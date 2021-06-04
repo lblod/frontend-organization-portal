8711943a978527a95b19fda6e4d2ee16b9d9097e
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-06-04 11:26:13 +0200

    Remove the controller code
    This shouldn't be needed if you use includes, or use async relationships to fetch the data.

--- app/controllers/people/index.js
+++ app/controllers/people/index.js
@@ -1,28 +1,3 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
 
-export default class PeopleIndexController extends Controller {
-  @tracked shortedModel;
-
-  constructor() {
-    super(...arguments);
-    this.getAdminUnitInfo()
-  }
-
-  async getAdminUnitInfo() {
-    //let shortedModel = [];
-
-    const mandatories = await this.model.mandatories;
-    const mandate = await mandatories[0].mandate;
-    const tempGoverningBody = await mandate.governingBody;
-    const governingBody = await tempGoverningBody.isTimeSpecializationOf;
-    const adminUnit = await governingBody.administrativeUnit;
-    const role = await mandate.roleBoard;
-
-    // TODO: Array to fill the table. Include only:  this.model.givenName, this.model.familyName, adminUnit.name and role.label
-        
-    //this.shortedModel = shortedModel
-    
-  }
-
-}
+export default class PeopleIndexController extends Controller {}
