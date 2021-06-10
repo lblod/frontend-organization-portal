10a20f81893093a43b96a0db51df9d030c3260af
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-06-10 16:13:53 +0200

    Add filters to the people page
    This allows users to filter on given name, family name or organisation

--- app/controllers/people/index.js
+++ app/controllers/people/index.js
@@ -1,3 +1,18 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
-export default class PeopleIndexController extends Controller {}
+export default class PeopleIndexController extends Controller {
+  @service router;
+  queryParams = ['givenName', 'familyName', 'organization'];
+
+  @tracked givenName = '';
+  @tracked familyName = '';
+  @tracked organization = '';
+
+  @action
+  search() {
+    this.router.refresh();
+  }
+}
--- app/routes/people/index.js
+++ app/routes/people/index.js
@@ -1,17 +1,30 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-// eslint-disable-next-line ember/no-mixins
-import DataTableRouteMixin from 'ember-data-table/mixins/route';
+export default class PeopleIndexRoute extends Route {
+  @service store;
 
-export default class PeopleIndexRoute extends Route.extend(DataTableRouteMixin){
-  modelName = 'person';
-
-  mergeQueryOptions() {
-    return {
+  model(params) {
+    let query = {
       include: [
+        'mandatories.status',
         'mandatories.mandate.governing-body.is-time-specialization-of.administrative-unit',
         'mandatories.mandate.role-board',
       ].join()
+    };
+
+    if (params.givenName) {
+      query['filter[given-name]'] = params.givenName;
     }
+
+    if (params.familyName) {
+      query['filter[family-name]'] = params.familyName;
+    }
+
+    if (params.organization) {
+      query['filter[mandatories][mandate][governing-body][is-time-specialization-of][administrative-unit][name]'] = params.organization;
+    }
+
+    return this.store.query('person', query);
   }
 }
