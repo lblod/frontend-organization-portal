d4b7392606ea13cfdd33b971473a630a5b9484b4
gcapurso <giovanna.capurso@gmail.com>
2021-06-14 19:57:46 +0200

    Add search logic

--- app/controllers/administrative-units/index.js
+++ app/controllers/administrative-units/index.js
@@ -1,3 +1,54 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
+<<<<<<< HEAD
 export default class AdministrativeUnitsIndexController extends Controller {}
+=======
+export default class AdministrativeUnitsIndexController extends Controller {
+    @service router;
+    queryParams = ['name', 'municipality', 'classification', 'organizationStatus'];
+
+    @tracked name = '';
+    @tracked municipality = '';
+    @tracked classification = '';
+    @tracked organizationStatus = '';
+
+    @action
+    search() {
+        this.router.refresh();
+    }
+
+    @action
+    setClassification(selection) {
+        this.classification = selection.id;
+    }
+
+    get selectedClassification() {
+        if (!this.classification) {
+            return null;
+        }
+
+        return this.model.classifications.find((classification) => {
+            return classification.id === this.classification;
+        });
+    }
+
+    @action
+    setOrganizationStatus(selection) {
+        this.organizationStatus = selection.id;
+    }
+
+
+    get selectedOrganizationStatus() {
+        if (!this.organizationStatus) {
+            return null;
+        }
+
+        return this.model.statuses.find((organizationStatus) => {
+            return organizationStatus.id === this.organizationStatus;
+        });
+    }
+}
+>>>>>>> Add search logic
--- app/routes/administrative-units/index.js
+++ app/routes/administrative-units/index.js
@@ -1,10 +1,44 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
-// eslint-disable-next-line ember/no-mixins
-import DataTableRouteMixin from 'ember-data-table/mixins/route';
+export default class AdministrativeUnitsIndexRoute extends Route {
+  @service store;
 
+<<<<<<< HEAD
 export default class AdministrativeUnitsIndexRoute extends Route.extend(
   DataTableRouteMixin
 ) {
   modelName = 'administrative-unit';
 }
+=======
+  model(params) {
+  
+    let query = {
+    };
+
+    if (params.name) {
+      query['filter[name]'] = params.name;
+    }
+
+    if (params.classification) {
+      query['filter[classification][:id:]'] = params.classification;
+    }
+
+    if (params.municipality) {
+      query['filter[primary-site][address][municipality]'] = params.municipality;
+    }
+
+    if (params.organizationStatus) {
+      query['filter[organization-status][:id:]'] = params.organizationStatus;
+    }
+
+    return hash({
+      classifications: this.store.findAll('administrative-unit-classification-code'),
+      statuses: this.store.findAll('organization-status-code'),
+      administrativeUnit: this.store.query('administrative-unit', query)
+    });
+
+  }
+}
+>>>>>>> Add search logic
