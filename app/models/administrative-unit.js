a38855e27385266f70130ed0e5635962e436e980
gcapurso <giovanna.capurso@gmail.com>
2021-06-14 10:54:07 +0200

    Update model

--- app/models/administrative-unit.js
+++ app/models/administrative-unit.js
@@ -2,7 +2,15 @@ import { hasMany, belongsTo } from '@ember-data/model';
 import OrganizationModel from './organization';
 
 export default class AdministrativeUnitModel extends OrganizationModel {
+<<<<<<< HEAD
   @belongsTo('administrative-unit-classification-code') classification;
   @hasMany('governing-body', { inverse: 'administrativeUnit' }) governingBodies;
   @hasMany('public-involvement', { inverse: null }) involvedBoards;
+=======
+    @belongsTo('administrative-unit-classification-code') classification;
+    @belongsTo('location') municipality;
+    @belongsTo('location') province;
+    @hasMany('governing-body', { inverse: 'administrativeUnit' }) governingBodies;
+    @hasMany('public-involvement', { inverse: null }) involvedBoards;
+>>>>>>> Update model
 }
