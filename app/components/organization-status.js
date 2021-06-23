cb8c77392dd6a0fe992d6f1183818ec38969b9ee
gcapurso <41680419+gcapurso@users.noreply.github.com>
2021-06-23 12:19:01 +0200

    Add administrative unit status colors (#32)

---
+++ app/components/organization-status.js
@@ -0,0 +1,13 @@
+import Component from '@glimmer/component';
+
+const ORGANIZATION_STATUS_SKINS = {
+  '63cc561de9188d64ba5840a42ae8f0d6': 'success',
+  abf4fee82019f88cf122f986830621ab: 'warning',
+  d02c4e12bf88d2fdf5123b07f29c9311: 'error',
+};
+
+export default class OrganizationStatusComponent extends Component {
+  get statusSkin() {
+    return ORGANIZATION_STATUS_SKINS[this.args.status.get('id')];
+  }
+}
