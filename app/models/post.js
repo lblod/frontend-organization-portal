cb801f543673dddc098ae37e71b46e5dedc179f2
gcapurso <giovanna.capurso@gmail.com>
2021-07-02 11:34:41 +0200

    Add position inverse relationship

--- app/models/post.js
+++ app/models/post.js
@@ -1,7 +1,8 @@
-import Model, { belongsTo } from '@ember-data/model';
+import Model, { belongsTo, hasMany } from '@ember-data/model';
 
 export default class PostModel extends Model {
   @belongsTo('board-position') roleBoard;
   @belongsTo('role') generalRole;
   @belongsTo('organization') organization;
+  @hasMany('agent-in-position', { inverse: 'position' }) agentsInPosition;
 }
