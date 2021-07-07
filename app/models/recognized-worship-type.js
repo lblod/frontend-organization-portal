e0e8db3e5bf841102ac36e36235205cc69802661
anitacaron <anitarcaron@gmail.com>
2021-07-07 16:21:44 +0100

    Define worship-administrative-unit model

--- app/models/recognized-worship-type.js
+++ app/models/recognized-worship-type.js
@@ -1,5 +1,5 @@
 import Model, { attr } from '@ember-data/model';
 
-export default class HonoraryServiceTypeModel extends Model {
+export default class RecognizedWorshipTypeModel extends Model {
   @attr label;
 }
