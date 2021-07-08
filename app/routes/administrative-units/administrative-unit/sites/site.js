9808dfcd7cb429186271e6f9ed896ea9afcb38ca
gcapurso <giovanna.capurso@gmail.com>
2021-07-08 16:29:33 +0200

    Fix siteId

--- app/routes/administrative-units/administrative-unit/sites/site.js
+++ app/routes/administrative-units/administrative-unit/sites/site.js
@@ -3,13 +3,9 @@ import { inject as service } from '@ember/service';
 export default class AdministrativeUnitsAdministrativeUnitSitesSiteRoute extends Route {
   @service store;
 
-  async model() {
+  async model({ siteId }) {
     let administrativeUnit = this.modelFor(
       'administrative-units.administrative-unit'
-    );
-
-    let { siteId: siteId } = this.paramsFor(
-      'administrative-units.administrative-unit.sites.site'
     );
 
     let site = await this.store.findRecord('site', siteId, {
