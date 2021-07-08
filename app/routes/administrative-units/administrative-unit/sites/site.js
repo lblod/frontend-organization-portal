bb42d454188f99b93f6463b155dea32064a6e67f
gcapurso <giovanna.capurso@gmail.com>
2021-07-08 16:07:49 +0200

    Fix breadcrumb

--- app/routes/administrative-units/administrative-unit/sites/site.js
+++ app/routes/administrative-units/administrative-unit/sites/site.js
@@ -1,3 +1,25 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+export default class AdministrativeUnitsAdministrativeUnitSitesSiteRoute extends Route {
+  @service store;
 
-export default class AdministrativeUnitsAdministrativeUnitSitesSiteRoute extends Route {}
+  async model() {
+    let administrativeUnit = this.modelFor(
+      'administrative-units.administrative-unit'
+    );
+
+    let { siteId: siteId } = this.paramsFor(
+      'administrative-units.administrative-unit.sites.site'
+    );
+
+    let site = await this.store.findRecord('site', siteId, {
+      reload: true,
+      include: ['address', 'contacts'].join(),
+    });
+
+    return {
+      site: site,
+      administrativeUnit: administrativeUnit,
+    };
+  }
+}
--- app/routes/administrative-units/administrative-unit/sites/site/index.js
+++ app/routes/administrative-units/administrative-unit/sites/site/index.js
@@ -1,26 +1,7 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
 export default class AdministrativeUnitsAdministrativeUnitSitesSiteIndexRoute extends Route {
-  @service store;
-
-  async model() {
-    let administrativeUnit = this.modelFor(
-      'administrative-units.administrative-unit'
-    );
-
-    let { siteId: siteId } = this.paramsFor(
-      'administrative-units.administrative-unit.sites.site'
-    );
-
-    let site = await this.store.findRecord('site', siteId, {
-      reload: true,
-      include: ['address', 'contacts'].join(),
-    });
-
-    return {
-      site: site,
-      administrativeUnit: administrativeUnit,
-    };
+  model() {
+    return this.modelFor('administrative-units.administrative-unit.sites.site');
   }
 }
