621dbc43604e015ae110f7751c724d199647004e
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-08-04 17:23:04 +0200

    Mock login page improvements

--- app/routes/mock-login.js
+++ app/routes/mock-login.js
@@ -2,8 +2,8 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class MockLoginRoute extends Route {
-  @service() session;
-  @service() store;
+  @service session;
+  @service store;
 
   queryParams = {
     page: {
@@ -15,12 +15,13 @@ export default class MockLoginRoute extends Route {
     this.session.prohibitAuthentication('index');
   }
 
-  model(params) {
-    const filter = { provider: 'https://github.com/lblod/mock-login-service' };
-    return this.store.query('account', {
+  async model(params) {
+    let accounts = await this.store.query('account', {
       include: 'user.groups',
-      filter: filter,
+      filter: { provider: 'https://github.com/lblod/mock-login-service' },
       page: { size: 10, number: params.page },
     });
+
+    return { accounts };
   }
 }
