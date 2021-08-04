b41757f740fb7ec5bf421f446ef09b4d721599ce
Nordine Bittich <nordine@bittich.be>
2021-08-04 11:27:10 +0200

    fine tuning

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class IndexRoute extends Route {}
+export default class IndexRoute extends Route {
+  @service session;
+
+  async beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'mock-login');
+  }
+}
