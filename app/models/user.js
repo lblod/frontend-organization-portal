df5311daaca7ccfedb5ef224398dc1252b315dcc
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-08-04 17:23:53 +0200

    Auth model updates - explicit inverses - sync relationships

--- app/models/user.js
+++ app/models/user.js
@@ -1,14 +1,23 @@
-import { get } from '@ember/object';
 import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class UserModel extends Model {
-  @attr() firstName;
-  @attr() familyName;
-  @hasMany('account') accounts;
-  @hasMany('group') groups;
+  @attr firstName;
+  @attr familyName;
+
+  @hasMany('account', {
+    inverse: 'user',
+    async: false,
+  })
+  accounts;
+
+  @hasMany('group', {
+    inverse: null,
+    async: false,
+  })
+  groups;
 
   get group() {
-    return get(this, 'groups.firstObject'); // eslint-disable-line ember/no-get
+    return this.groups.firstObject;
   }
 
   get fullName() {
