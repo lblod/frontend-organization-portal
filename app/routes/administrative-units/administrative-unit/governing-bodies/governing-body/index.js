c466d8e91344f22034a21e2e2e130779ea7a1621
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-08-11 14:27:56 +0200

    Move the mandate grouping logic
    This is only used on the index page. This also means that it will be rerun after adding a new mandate.

--- app/routes/administrative-units/administrative-unit/governing-bodies/governing-body/index.js
+++ app/routes/administrative-units/administrative-unit/governing-bodies/governing-body/index.js
@@ -1,9 +1,29 @@
 import Route from '@ember/routing/route';
 
+const MEMBER_ROLE_ID = '2e021095727b2464459a63e16ebeafd2';
+
 export default class AdministrativeUnitsAdministrativeUnitGoverningBodiesGoverningBodyIndexRoute extends Route {
-  model() {
-    return this.modelFor(
-      'administrative-units.administrative-unit.governing-bodies.governing-body'
-    );
+  async model() {
+    let { administrativeUnit, governingBodyClassification, governingBody } =
+      this.modelFor(
+        'administrative-units.administrative-unit.governing-bodies.governing-body'
+      );
+
+    let mandates = await governingBody.mandates;
+    let memberMandates = mandates.filter((mandate) => {
+      return mandate.get('roleBoard.id') === MEMBER_ROLE_ID;
+    });
+
+    let otherMandates = mandates.filter((mandate) => {
+      return mandate.get('roleBoard.id') !== MEMBER_ROLE_ID;
+    });
+
+    return {
+      administrativeUnit,
+      governingBodyClassification,
+      governingBody,
+      memberMandates,
+      otherMandates,
+    };
   }
 }
