72250d11b6134dac1fc1fc8c86fba6ad560961e5
Anita Caron <anitarcaron@gmail.com>
2021-08-17 10:23:46 +0100

    Change from minister/mandatory status component to position status component

--- app/components/position-status.js
+++ app/components/position-status.js
@@ -1,12 +1,12 @@
 import { assert } from '@ember/debug';
 import Component from '@glimmer/component';
 
-export default class MinisterStatusComponent extends Component {
-  get isActiveMinister() {
+export default class PositionStatusComponent extends Component {
+  get isActivePosition() {
     let endDate = this.args.endDate;
 
     if (!endDate) {
-      // No end date set, so the mandatory is still active
+      // No end date set, so the position is still active
       return true;
     } else {
       return isDateInTheFuture(endDate);
