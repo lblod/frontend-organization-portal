7ac90fd7d91ee35f36a6fda10879d07634963901
Anita Caron <anitarcaron@gmail.com>
2021-08-18 16:01:16 +0100

    Add sortable header to the table

--- app/routes/administrative-units/administrative-unit/local-involvements/index.js
+++ app/routes/administrative-units/administrative-unit/local-involvements/index.js
@@ -4,19 +4,26 @@ import { inject as service } from '@ember/service';
 export default class AdministrativeUnitsAdministrativeUnitLocalInvolvementsIndexRoute extends Route {
   @service store;
 
-  async model() {
+  queryParams = {
+    sort: { refreshModel: true },
+  };
+
+  async model(params) {
     let administrativeUnit = this.modelFor(
       'administrative-units.administrative-unit'
     );
 
-    let involvements = await this.store.query('local-involvement', {
+    let query = {
       include: 'involvement-type,administrative-unit.classification',
       filter: {
         ['worship-service']: {
           [':id:']: administrativeUnit.id,
         },
       },
-    });
+      sort: params.sort,
+    };
+
+    let involvements = await this.store.query('local-involvement', query);
 
     return {
       administrativeUnit,
