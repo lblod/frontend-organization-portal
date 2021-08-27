23ca7c866818e789cb442b921f9db1ef5c4a5e8e
Anita Caron <anitarcaron@gmail.com>
2021-08-27 14:30:38 +0100

    Link to issue in the data table

--- app/utils/data-table.js
+++ app/utils/data-table.js
@@ -1,6 +1,7 @@
 // AuDataTable expects arrays to have a meta.count property for the pagination to work correctly.
 // This meta object is automatically set by the serializer, but only for data that passes through Ember Data.
 // This util can be used to add the proper count to arrays that are created client side.
+// Issue created here https://github.com/appuniversum/ember-appuniversum/issues/136
 export function addPaginationMeta(recordArray) {
   recordArray.meta = {
     get count() {
