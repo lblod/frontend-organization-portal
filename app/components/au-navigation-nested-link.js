cfd9aaed3ca7ae205d87f095dd9f4c13c9bca478
Dieter Peirs <github@dieterpeirs.com>
2021-08-27 09:54:27 +0200

    improve tab focus

---
+++ app/components/au-navigation-nested-link.js
@@ -0,0 +1,29 @@
+import Component from '@glimmer/component';
+import { deprecate } from '@ember/debug';
+import { action } from '@ember/object';
+
+export default class AuNavigationNestedLink extends Component {
+  get route() {
+    if (this.args.linkRoute) {
+      deprecate('@linkRoute is deprecated, use @route instead', false, {
+        id: '@appuniversum/ember-appuniversum.au-navigation-link.linkRoute-argument',
+        until: '0.7.0',
+        for: '@appuniversum/ember-appuniversum',
+        since: {
+          enabled: '0.5.0',
+        },
+      });
+
+      return this.args.linkRoute;
+    } else if (this.args.route) {
+      return this.args.route;
+    } else {
+      return undefined;
+    }
+  }
+  @action
+  linkFocus() {
+    // Focus content window
+    document.querySelectorAll('#container')[0].focus();
+  }
+}
