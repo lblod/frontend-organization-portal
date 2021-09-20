e0227d2bdc14de64b16e0de0107948edfdf44733
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-09-20 11:27:04 +0200

    Add validations to the personal information edit page

--- app/routes/people/person/personal-information/edit.js
+++ app/routes/people/person/personal-information/edit.js
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
+import { createValidatedChangeset } from 'frontend-contact-hub/utils/changeset';
+import personValidations from 'frontend-contact-hub/validations/person';
 
 export default class PeoplePersonPersonalInformationEditRoute extends Route {
   model() {
-    return this.modelFor('people.person.personal-information');
+    let { person } = this.modelFor('people.person.personal-information');
+
+    return {
+      person: createValidatedChangeset(person, personValidations),
+    };
   }
 }
