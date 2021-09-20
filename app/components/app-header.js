5f8d911ce14e5841c4fc8bef852db0d5e0998cfc
Dieter Peirs <github@dieterpeirs.com>
2021-09-20 16:47:25 +0200

    update ember-appuniversum, add environment pill

--- app/components/app-header.js
+++ app/components/app-header.js
@@ -2,9 +2,28 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
+const environmentNames = {
+  localhost: 'LOCAL',
+  'dev.organisaties.abb.lblod.info': 'DEV',
+  'organisaties.abb.lblod.info': 'QA',
+};
+
 export default class AppHeaderComponent extends Component {
   @service currentSession;
   @service session;
+
+  constructor() {
+    super(...arguments);
+
+    const hostname = window.location.hostname;
+    if (environmentNames[hostname]) {
+      this.environmentName = environmentNames[hostname];
+      this.environmentClass = `au-c-environment-pill--${this.environmentName.toLowerCase()}`;
+    } else {
+      this.environmentName = null;
+      this.environmentClass = null;
+    }
+  }
 
   @action
   logout() {
