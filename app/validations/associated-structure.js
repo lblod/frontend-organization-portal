49f65b114e08dd7aa7c6e30480530e0054f9b010
gcapurso <giovanna.capurso@gmail.com>
2021-10-05 09:43:06 +0200

    Add rebase

--- app/validations/associated-structure.js
+++ app/validations/associated-structure.js
@@ -7,7 +7,3 @@ export default {
 export const legalTypeValidations = {
   label: validatePresence({ presence: true, ignoreBlank: true }),
 };
-
-export const legalTypeValidations = {
-  label: validatePresence({ presence: true, ignoreBlank: true }),
-};
