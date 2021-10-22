b90f5ea5851032cb373c088f2119cd0327193946
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-10-22 11:24:59 +0200

    Update the browser targets to the latest blueprint version

--- config/targets.js
+++ config/targets.js
@@ -1,21 +1,10 @@
 'use strict';
 
-let browsers = [
-  'Chrome >= 78',
+const browsers = [
+  'last 1 Chrome versions',
   'last 1 Firefox versions',
   'last 1 Safari versions',
 ];
-
-const isCI = Boolean(process.env.CI);
-const isProduction = process.env.EMBER_ENV === 'production';
-
-if (isCI || isProduction) {
-  browsers = [
-    'Chrome >= 78',
-    'last 3 Firefox versions',
-    'last 3 Safari versions',
-  ];
-}
 
 module.exports = {
   browsers,
