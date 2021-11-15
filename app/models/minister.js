d596c0d3fbc99544bd6a76d8e4d6a12202a104b0
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-11-15 19:43:49 +0100

    Add validations to the minister creation page

--- app/models/minister.js
+++ app/models/minister.js
@@ -1,7 +1,11 @@
 import { hasMany, belongsTo } from '@ember-data/model';
 import AgentInPositionModel from './agent-in-position';
+import { tracked } from '@glimmer/tracking';
 
 export default class MinisterModel extends AgentInPositionModel {
+  // used for validations
+  @tracked isCurrentPosition;
+
   @belongsTo('minister-position', {
     inverse: null,
   })
---
+++ app/validations/minister.js
@@ -0,0 +1,36 @@
+import { validatePresence } from 'ember-changeset-validations/validators';
+import { validateConditionally } from 'frontend-contact-hub/validators/validate-conditionally';
+
+export default {
+  agentStartDate: validatePresence({
+    presence: true,
+    ignoreBlank: true,
+    message: 'Vul de startdatum in',
+  }),
+  agentEndDate: validateConditionally(
+    validatePresence({
+      presence: true,
+      ignoreBlank: true,
+      message: 'Vul de einddatum in',
+    }),
+    function (changes, content) {
+      let requiresAgentEndDate;
+      let isCurrentPosition = changes.isCurrentPosition;
+
+      if (typeof isCurrentPosition !== 'boolean') {
+        isCurrentPosition = content.isCurrentPosition;
+      }
+
+      requiresAgentEndDate = !isCurrentPosition;
+      return requiresAgentEndDate;
+    }
+  ),
+};
+
+export const positionValidations = {
+  function: validatePresence({
+    presence: true,
+    ignoreBlank: true,
+    message: 'Selecteer een positie.',
+  }),
+};
