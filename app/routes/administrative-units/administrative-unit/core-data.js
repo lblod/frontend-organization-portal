61a77be945a986a4ff0837fdebcc2ad11cdfd160
Nordine Bittich <nordine@bittich.be>
2021-12-06 17:31:37 +0100

    wip

--- app/routes/administrative-units/administrative-unit/core-data.js
+++ app/routes/administrative-units/administrative-unit/core-data.js
@@ -4,30 +4,36 @@ import { inject as service } from '@ember/service';
 export default class AdministrativeUnitsAdministrativeUnitCoreDataRoute extends Route {
   @service store;
 
-  model() {
+  async model() {
     let { id: administrativeUnitId } = this.paramsFor(
       'administrative-units.administrative-unit'
     );
-
-    return this.store.findRecord(
-      'worship-administrative-unit',
-      administrativeUnitId,
-      {
-        reload: true,
-        include: [
-          'classification',
-          'organization-status',
-          'recognized-worship-type',
-          'scope',
-          'identifiers.structured-identifier',
-          'primary-site.address',
-          'primary-site.contacts',
-          'is-sub-organization-of',
-          'is-associated-with',
-          'sub-organizations',
-          'resulted-from',
-        ].join(),
-      }
-    );
+    return {
+      unit: await this.store.findRecord(
+        'worship-administrative-unit',
+        administrativeUnitId,
+        {
+          reload: true,
+          include: [
+            'classification',
+            'organization-status',
+            'recognized-worship-type',
+            'scope',
+            'identifiers.structured-identifier',
+            'primary-site.address',
+            'primary-site.contacts',
+            'is-sub-organization-of',
+            'is-associated-with',
+            //'sub-organizations',
+            'resulted-from',
+          ].join(),
+        }
+      ),
+      subOrganizations: await this.store.query('organization', {
+        include: 'classification',
+        ['filter[is-sub-organization-of][:id:]']: administrativeUnitId,
+        ['page[size]']: 500,
+      }),
+    };
   }
 }
