ba5b5292a3972bab43de8de359434b28df42f9d4
Nordine Bittich <nordine@bittich.be>
2021-12-07 10:23:41 +0100

    bugfix

--- app/routes/administrative-units/administrative-unit/core-data.js
+++ app/routes/administrative-units/administrative-unit/core-data.js
@@ -4,36 +4,29 @@ import { inject as service } from '@ember/service';
 export default class AdministrativeUnitsAdministrativeUnitCoreDataRoute extends Route {
   @service store;
 
-  async model() {
+  model() {
     let { id: administrativeUnitId } = this.paramsFor(
       'administrative-units.administrative-unit'
     );
-    return {
-      unit: await this.store.findRecord(
-        'worship-administrative-unit',
-        administrativeUnitId,
-        {
-          reload: true,
-          include: [
-            'classification',
-            'organization-status',
-            'recognized-worship-type',
-            'scope',
-            'identifiers.structured-identifier',
-            'primary-site.address',
-            'primary-site.contacts',
-            'is-sub-organization-of',
-            'is-associated-with',
-            //'sub-organizations',
-            'resulted-from',
-          ].join(),
-        }
-      ),
-      subOrganizations: await this.store.query('organization', {
-        include: 'classification',
-        ['filter[is-sub-organization-of][:id:]']: administrativeUnitId,
-        ['page[size]']: 500,
-      }),
-    };
+    return this.store.findRecord(
+      'worship-administrative-unit',
+      administrativeUnitId,
+      {
+        reload: true,
+        include: [
+          'classification',
+          'organization-status',
+          'recognized-worship-type',
+          'scope',
+          'identifiers.structured-identifier',
+          'primary-site.address',
+          'primary-site.contacts',
+          'is-sub-organization-of',
+          'is-associated-with',
+          //'sub-organizations',
+          'resulted-from',
+        ].join(),
+      }
+    );
   }
 }
--- app/routes/administrative-units/administrative-unit/core-data/index.js
+++ app/routes/administrative-units/administrative-unit/core-data/index.js
@@ -2,10 +2,16 @@ import Route from '@ember/routing/route';
 
 export default class AdministrativeUnitsAdministrativeUnitCoreDataIndexRoute extends Route {
   model() {
+    const { id } = this.paramsFor('administrative-units.administrative-unit');
     return {
       administrativeUnit: this.modelFor(
         'administrative-units.administrative-unit.core-data'
       ),
+      subOrganizations: this.store.query('organization', {
+        include: 'classification',
+        'filter[is-sub-organization-of][:id:]': id,
+        'page[size]': 500,
+      }),
     };
   }
 }
