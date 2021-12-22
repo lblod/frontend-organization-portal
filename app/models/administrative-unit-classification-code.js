1a1684eac4c9a0524f2fd9affdbd9e1a69a3d813
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-12-22 11:12:30 +0100

    `AdministrativeUnitSelect` component improvements
    These changes make the component more generic so that it can be used on other pages.

--- app/models/administrative-unit-classification-code.js
+++ app/models/administrative-unit-classification-code.js
@@ -1,5 +1,10 @@
 import Model, { attr } from '@ember-data/model';
 
+export const CLASSIFICATION_CODE = {
+  MUNICIPALITY: '5ab0e9b8a3b2ca7c5e000001',
+  PROVINCE: '5ab0e9b8a3b2ca7c5e000000',
+};
+
 export default class AdministrativeUnitClassificationCodeModel extends Model {
   @attr label;
 }
