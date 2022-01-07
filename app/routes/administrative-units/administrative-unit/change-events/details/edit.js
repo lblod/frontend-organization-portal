18d95630f2b81bb2533d0a7697b1581f46cf1fd2
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-01-07 14:22:08 +0100

    Implement the change event edit page

---
+++ app/routes/administrative-units/administrative-unit/change-events/details/edit.js
@@ -0,0 +1,42 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { CHANGE_EVENT_TYPE } from 'frontend-contact-hub/models/change-event-type';
+import { createValidatedChangeset } from 'frontend-contact-hub/utils/changeset';
+import {
+  changeEventValidations,
+  decisionValidations,
+} from 'frontend-contact-hub/validations/change-event';
+
+export default class AdministrativeUnitsAdministrativeUnitChangeEventsDetailsEditRoute extends Route {
+  @service currentSession;
+  @service router;
+  @service store;
+
+  beforeModel() {
+    if (!this.currentSession.canEdit) {
+      this.router.transitionTo('route-not-found', {
+        wildcard: 'pagina-niet-gevonden',
+      });
+    }
+  }
+
+  async model() {
+    let { changeEvent, ...detailsPageModel } = this.modelFor(
+      'administrative-units.administrative-unit.change-events.details'
+    );
+
+    let changeEventType = await changeEvent.type;
+    let decision = await changeEvent.decision;
+
+    return {
+      ...detailsPageModel,
+      changeEvent: createValidatedChangeset(
+        changeEvent,
+        changeEventValidations
+      ),
+      decision: createValidatedChangeset(decision, decisionValidations),
+      canAddDecisionInformation:
+        changeEventType.id !== CHANGE_EVENT_TYPE.RECOGNITION_REQUESTED,
+    };
+  }
+}
