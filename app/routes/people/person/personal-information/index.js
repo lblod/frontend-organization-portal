ec6a130648011e7d4ed49b21bcd4c10677c8efff
Nordine Bittich <nordine@bittich.be>
2022-01-28 11:31:06 +0100

    bugfix

--- app/routes/people/person/personal-information/index.js
+++ app/routes/people/person/personal-information/index.js
@@ -1,7 +1,35 @@
 import Route from '@ember/routing/route';
+import { assert } from '@ember/debug';
 
 export default class PeoplePersonPersonalInformationIndexRoute extends Route {
-  model() {
-    return this.modelFor('people.person.personal-information');
+  async model() {
+    const personInfo = await this.modelFor(
+      'people.person.personal-information'
+    );
+    const mandatories = personInfo.person.mandatories
+      .filter(this.isActivePosition)
+      .sort((a, b) => {
+        return new Date(b.startDate) - new Date(a.startDate);
+      });
+    return {
+      person: personInfo.person,
+      mandatories: mandatories,
+    };
   }
+
+  isActivePosition(mandatory) {
+    let endDate = mandatory.endDate;
+    if (!endDate) {
+      // No end date set, so the position is still active
+      return true;
+    } else {
+      return isDateInTheFuture(endDate);
+    }
+  }
+}
+function isDateInTheFuture(date) {
+  assert('endDate should be a Date instance', date instanceof Date);
+  let today = new Date();
+
+  return date - today >= 0;
 }
