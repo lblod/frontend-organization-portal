b8cbd196300ee125e130a40e02198d54a3f499d2
Nordine Bittich <nordine@bittich.be>
2022-02-03 09:28:37 +0100

    position select component

---
+++ app/components/position-select.js
@@ -0,0 +1,34 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
+
+export default class PositionSelectComponent extends Component {
+  @service store;
+
+  constructor() {
+    super(...arguments);
+
+    this.loadPositionTask.perform();
+  }
+
+  get selectedPosition() {
+    if (typeof this.args.selected === 'string') {
+      return this.findPositionById(this.args.selected);
+    }
+
+    return this.args.selected;
+  }
+
+  findPositionById(id) {
+    if (this.loadPositionTask.isRunning) {
+      return null;
+    }
+
+    let position = this.loadPositionTask.last.value;
+    return position.find((p) => p.id === id);
+  }
+
+  @task *loadPositionTask() {
+    return yield this.store.findAll('board-position');
+  }
+}
