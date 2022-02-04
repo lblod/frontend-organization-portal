769b2cab1a638cec76855f617abeb5c2e9d4f883
Nordine Bittich <3816305+nbittich@users.noreply.github.com>
2022-02-04 10:35:58 +0100

    People search page improvements (#254)
    This replaces the `mu-cl-resources` based searching with `mu-search` which has several benefits:
    
    - It's FAST. `mu-cl-resources` isn't made for highly interactive search interfaces, so most actions took a long time. The new implementation doesn't have this problem.
    - It's possible to show all positions instead of only the first one
    - Better filtering technologies which makes fuzzy searching possible

--- app/components/administrative-unit-select.js
+++ app/components/administrative-unit-select.js
@@ -1,10 +1,15 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { restartableTask, timeout } from 'ember-concurrency';
+import { restartableTask, timeout, task } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
 
 export default class AdministrativeUnitSelectComponent extends Component {
   @service store;
-
+  @tracked loadedRecord;
+  constructor() {
+    super(...arguments);
+    this.loadRecord.perform();
+  }
   @restartableTask
   *loadAdministrativeUnitsTask(searchParams = '') {
     yield timeout(500);
@@ -31,6 +36,25 @@ export default class AdministrativeUnitSelectComponent extends Component {
       return this.args.filter(searchResults);
     } else {
       return searchResults;
+    }
+  }
+
+  get selectedAdministrativeUnit() {
+    if (typeof this.args.selected === 'string') {
+      return this.loadedRecord;
+    }
+
+    return this.args.selected;
+  }
+
+  @task
+  *loadRecord() {
+    let selectedOrganization = this.args.selected;
+    if (typeof selectedOrganization === 'string') {
+      this.loadedRecord = yield this.store.findRecord(
+        'administrative-unit',
+        selectedOrganization
+      );
     }
   }
 }
---
+++ app/services/mu-search.js
@@ -0,0 +1,93 @@
+import Service from '@ember/service';
+import fetch from 'fetch';
+import ArrayProxy from '@ember/array/proxy';
+import { A } from '@ember/array';
+
+export default class MuSearchService extends Service {
+  sortOrder(sort) {
+    if (sort.startsWith('-')) {
+      return 'desc';
+    }
+    if (sort.length > 0) {
+      return 'asc';
+    }
+    return null;
+  }
+  stripSort(sort) {
+    return sort.replace(/(^\+)|(^-)/g, '');
+  }
+
+  getPaginationMetadata(pageNumber, size, total) {
+    const pagination = {};
+
+    pagination.first = {
+      number: 0,
+      size,
+    };
+
+    const lastPageNumber =
+      total % size === 0
+        ? Math.floor(total / size) - 1
+        : Math.floor(total / size);
+    const lastPageSize = total % size === 0 ? size : total % size;
+    pagination.last = {
+      number: lastPageNumber,
+      size: lastPageSize,
+    };
+
+    pagination.self = {
+      number: pageNumber,
+      size,
+    };
+
+    if (pageNumber > 0) {
+      pagination.prev = {
+        number: pageNumber - 1,
+        size,
+      };
+    }
+
+    if (pageNumber < lastPageNumber) {
+      const nextPageSize =
+        pageNumber + 1 === lastPageNumber ? lastPageSize : size;
+      pagination.next = {
+        number: pageNumber + 1,
+        size: nextPageSize,
+      };
+    }
+
+    return pagination;
+  }
+
+  async search(request) {
+    let { index, page, size, sort, filters, dataMapping } = request;
+    const params = [];
+    params.push(`page[size]=${size}`);
+    params.push(`page[number]=${page}`);
+
+    for (const field in filters) {
+      let q = filters[field];
+      let f = field;
+      params.push(`filter[${f}]=${q}`);
+    }
+
+    if (sort) {
+      params.push(
+        `sort[${this.stripSort(sort)}.field]=${this.sortOrder(sort)}`
+      );
+    }
+
+    const endpoint = `/search/${index}/search?${params.join('&')}`;
+    const { count, data } = await (await fetch(endpoint)).json();
+    const pagination = this.getPaginationMetadata(page, size, count);
+    const entries = A(data.map(dataMapping));
+
+    return ArrayProxy.create({
+      content: entries,
+      meta: {
+        count,
+        pagination,
+      },
+    });
+  }
+}
