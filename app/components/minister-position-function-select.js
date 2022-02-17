4d306d07d5e856ae7c7b2c6fcaa37dd3d3ed40e3
Nordine Bittich <nordine@bittich.be>
2022-02-17 16:40:36 +0100

    pr

--- app/components/minister-position-function-select.js
+++ app/components/minister-position-function-select.js
@@ -1,35 +1,13 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { task } from 'ember-concurrency';
-import { tracked } from '@glimmer/tracking';
 
 export default class MinisterPositionFunctionSelectComponent extends Component {
   @service store;
-  @tracked loadedRecord;
 
   constructor() {
     super(...arguments);
-    this.loadRecord.perform();
-
     this.fetchMinisterPositionFunctionsTask.perform();
-  }
-  get selectedMinisterPositionFunction() {
-    if (typeof this.args.selected === 'string') {
-      return this.loadedRecord;
-    }
-
-    return this.args.selected;
-  }
-
-  @task
-  *loadRecord() {
-    let selectedRole = this.args.selected;
-    if (typeof selectedRole === 'string') {
-      this.loadedRecord = yield this.store.findRecord(
-        'minister-position-function',
-        selectedRole
-      );
-    }
   }
 
   @task *fetchMinisterPositionFunctionsTask() {
