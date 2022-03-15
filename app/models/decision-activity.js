25ba5839b76afff3223f7669408a3ee3cf5333b7
Claire Lovisa <lovisa.claire@gmail.com>
2022-03-15 10:04:00 +0100

    Add missing model

---
+++ app/models/decision-activity.js
@@ -0,0 +1,10 @@
+import Model, { attr, hasMany } from '@ember-data/model';
+
+export default class DecisionActivityModel extends Model {
+  @attr('date') endDate;
+
+  @hasMany('decision', {
+    inverse: 'hasDecisionActivity',
+  })
+  givesCauseTo;
+}
