2222b3f6143e1f696f10293361b317cda8dc1ce8
Claire Lovisa <lovisa.claire@gmail.com>
2022-03-16 17:58:35 +0100

    Optimize query param use

--- app/controllers/organizations/organization/related-organizations.js
+++ app/controllers/organizations/organization/related-organizations.js
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 
 export default class OrganizationsOrganizationRelatedOrganizationsController extends Controller {
-  queryParams = ['sort', 'page'];
+  queryParams = ['sort', 'page', 'size'];
 
   @tracked sort = 'name';
   @tracked page = 0;
