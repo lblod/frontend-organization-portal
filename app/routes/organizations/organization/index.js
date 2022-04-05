6cf3ac54dc6de0e42e179927cd81c7020782d293
Claire Lovisa <lovisa.claire@gmail.com>
2022-04-05 17:42:24 +0200

    Redirect org index to core data

--- app/routes/organizations/organization/index.js
+++ app/routes/organizations/organization/index.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class OrganizationsOrganizationIndexRoute extends Route {
-  model() {
-    return this.modelFor('organizations.organization');
+  @service router;
+
+  beforeModel() {
+    return this.router.replaceWith('organizations.organization.core-data');
   }
 }
