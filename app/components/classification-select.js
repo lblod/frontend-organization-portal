56fc0de1fe15d5d2492db07b7912fdad4ec4491a
Claire Lovisa <lovisa.claire@gmail.com>
2022-04-07 16:32:29 +0200

    Add link type bestuur / type religion in filter

--- app/components/classification-select.js
+++ app/components/classification-select.js
@@ -1,20 +1,24 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { task } from 'ember-concurrency';
+import { useTask } from 'ember-resources';
 
 const CLASSIFICATION = {
   CENTRAL_WORSHIP_SERVICE: 'f9cac08a-13c1-49da-9bcb-f650b0604054',
   WORSHIP_SERVICE: '66ec74fd-8cfc-4e16-99c6-350b35012e86',
 };
+const CENTRAL_WORSHIP_SERVICE_BLACKLIST = [
+  '1a1abeafc973d27cebcb2d7a15b2d823', // Israëlitisch
+  '99536dd6eb0d2ef38a89efafb17e7389', // Anglicaans
+  'e8cba1540b35a32e9cb45126c38c03c6', // Protestants
+];
 
 export default class ClassificationSelectComponent extends Component {
   @service store;
 
-  constructor() {
-    super(...arguments);
-
-    this.loadClassificationsTask.perform();
-  }
+  classifications = useTask(this, this.loadClassificationsTask, () => [
+    this.args.selectedRecognizedWorshipTypeId,
+  ]);
 
   get selectedClassification() {
     if (typeof this.args.selected === 'string') {
@@ -25,20 +29,41 @@ export default class ClassificationSelectComponent extends Component {
   }
 
   findClassificationById(id) {
-    if (this.loadClassificationsTask.isRunning) {
+    if (this.classifications.isRunning) {
       return null;
     }
 
-    let classifications = this.loadClassificationsTask.last.value;
+    let classifications = this.classifications.value;
     return classifications.find((status) => status.id === id);
   }
 
   @task *loadClassificationsTask() {
-    return yield this.store.query('administrative-unit-classification-code', {
-      'filter[:id:]': [
+    // Trick used to avoid infinite loop
+    // See https://github.com/NullVoxPopuli/ember-resources/issues/340 for more details
+    yield Promise.resolve();
+
+    let allowedIds;
+    let selectedRecognizedWorshipTypeId =
+      this.args.selectedRecognizedWorshipTypeId;
+
+    if (
+      selectedRecognizedWorshipTypeId &&
+      this.isIdInBlacklist(selectedRecognizedWorshipTypeId)
+    ) {
+      allowedIds = [CLASSIFICATION.WORSHIP_SERVICE];
+    } else {
+      allowedIds = [
         CLASSIFICATION.WORSHIP_SERVICE,
         CLASSIFICATION.CENTRAL_WORSHIP_SERVICE,
-      ].join(),
+      ];
+    }
+
+    return yield this.store.query('administrative-unit-classification-code', {
+      'filter[:id:]': allowedIds.join(),
     });
+  }
+
+  isIdInBlacklist(id) {
+    return CENTRAL_WORSHIP_SERVICE_BLACKLIST.find((element) => element == id);
   }
 }
--- app/components/recognized-worship-type-select.js
+++ app/components/recognized-worship-type-select.js
@@ -1,15 +1,23 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { task } from 'ember-concurrency';
+import { useTask } from 'ember-resources';
+
+const CENTRAL_WORSHIP_SERVICE_ID = 'f9cac08a-13c1-49da-9bcb-f650b0604054';
+const CENTRAL_WORSHIP_SERVICE_BLACKLIST = [
+  '1a1abeafc973d27cebcb2d7a15b2d823', // Israëlitisch
+  '99536dd6eb0d2ef38a89efafb17e7389', // Anglicaans
+  'e8cba1540b35a32e9cb45126c38c03c6', // Protestants
+];
 
 export default class RecognizedWorshipTypeSelect extends Component {
   @service store;
 
-  constructor(...args) {
-    super(...args);
-
-    this.loadRecognizedWorshipTypesTask.perform();
-  }
+  recognizedWorshipTypes = useTask(
+    this,
+    this.loadRecognizedWorshipTypesTask,
+    () => [this.args.selectedClassificationId]
+  );
 
   get selectedRecognizedWorshipType() {
     if (typeof this.args.selected === 'string') {
@@ -20,16 +28,36 @@ export default class RecognizedWorshipTypeSelect extends Component {
   }
 
   getRecognizedWorshipTypeById(id) {
-    if (this.loadRecognizedWorshipTypesTask.isRunning) {
+    if (this.recognizedWorshipTypes.isRunning) {
       return null;
     }
 
-    let recognizedWorshipTypes = this.loadRecognizedWorshipTypesTask.last.value;
-    return recognizedWorshipTypes.find((worshipType) => worshipType.id === id);
+    return this.recognizedWorshipTypes.value.find(
+      (worshipType) => worshipType.id === id
+    );
   }
 
   @task
   *loadRecognizedWorshipTypesTask() {
-    return yield this.store.findAll('recognized-worship-type');
+    // Trick used to avoid infinite loop
+    // See https://github.com/NullVoxPopuli/ember-resources/issues/340 for more details
+    yield Promise.resolve();
+
+    let recognizedWorshipTypes = yield this.store.findAll(
+      'recognized-worship-type'
+    );
+
+    if (this.args.selectedClassificationId == CENTRAL_WORSHIP_SERVICE_ID) {
+      // Filter out blacklisted types for central worship services
+      recognizedWorshipTypes = recognizedWorshipTypes.filter(
+        (t) => !this.isIdInBlacklist(t.id)
+      );
+    }
+
+    return recognizedWorshipTypes;
+  }
+
+  isIdInBlacklist(id) {
+    return CENTRAL_WORSHIP_SERVICE_BLACKLIST.find((element) => element == id);
   }
 }
