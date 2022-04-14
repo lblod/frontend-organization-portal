00545e7119a095155859dc5d4f5c0e83e10e717d
Claire Lovisa <lovisa.claire@gmail.com>
2022-04-14 08:19:03 +0200

    Increase size page when filtering municipalities

--- app/components/municipality-select.js
+++ app/components/municipality-select.js
@@ -26,6 +26,9 @@ export default class MunicipalitySelectComponent extends Component {
           },
         },
         sort: 'name',
+        page: {
+          size: 400,
+        },
       });
 
       return municipalities.mapBy('name');
