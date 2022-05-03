1f24d091152973e689b79025c5f03f95775c9b77
Nordine Bittich <nordine@bittich.be>
2022-05-03 13:43:19 +0200

    validate mandatory date

--- app/validations/mandatory.js
+++ app/validations/mandatory.js
@@ -15,17 +15,35 @@ const mandatoryValidations = {
       return isWorshipMember(role?.id);
     }
   ),
-  startDate: validatePresence({
-    presence: true,
-    ignoreBlank: true,
-    message: 'Vul de startdatum in',
-  }),
-  endDate: validateConditionally(
+  expectedEndDate: (key, newValue, oldValue, changes, content) => {
+    if (!newValue) {
+      return true; // not mandatory
+    }
+    return validateEndDateAfterStartDate(
+      key,
+      newValue,
+      oldValue,
+      changes,
+      content
+    );
+  },
+  startDate: [
     validatePresence({
       presence: true,
       ignoreBlank: true,
-      message: 'Vul de effectieve einddatum in',
+      message: 'Vul de startdatum in',
     }),
+    validateStartDateBeforeEndDate,
+  ],
+  endDate: validateConditionally(
+    [
+      validatePresence({
+        presence: true,
+        ignoreBlank: true,
+        message: 'Vul de effectieve einddatum in',
+      }),
+      validateEndDateAfterStartDate,
+    ],
     function (changes, content) {
       let requiresEndDate;
       let isCurrentPosition = changes.isCurrentPosition;
@@ -42,6 +60,52 @@ const mandatoryValidations = {
 
 export default mandatoryValidations;
 
+function validateEndDateAfterStartDate(
+  key,
+  newValue,
+  oldValue,
+  changes,
+  content
+) {
+  const endDateValue = newValue || oldValue;
+  const endDate = new Date(endDateValue);
+  const newStartDateValue = changes?.startDate || content.startDate;
+
+  if (newStartDateValue) {
+    const newStartDate = new Date(newStartDateValue);
+    if (newStartDate.getTime() > endDate.getTime()) {
+      return 'Kies een einddatum die na de begindatum plaatsvindt';
+    }
+  }
+  return true;
+}
+function validateStartDateBeforeEndDate(
+  key,
+  newValue,
+  oldValue,
+  changes,
+  content
+) {
+  // The messages should also appear when the content of one of the fields is deleted.
+  const effectiveEndDateValue = changes?.endDate || content.endDate;
+  const expectedEndDateValue =
+    changes?.expectedEndDate || content.expectedEndDate;
+  const newStartDate = new Date(newValue);
+
+  if (effectiveEndDateValue) {
+    const effectiveEndDate = new Date(effectiveEndDateValue);
+    if (newStartDate.getTime() > effectiveEndDate.getTime()) {
+      return 'Kies een startdatum die na de begindatum plaatsvindt';
+    }
+  }
+  if (expectedEndDateValue) {
+    const expectedEndDate = new Date(expectedEndDateValue);
+    if (newStartDate.getTime() > expectedEndDate.getTime()) {
+      return 'Kies een startdatum die na de begindatum plaatsvindt';
+    }
+  }
+  return true;
+}
 export const mandatoryWithRequiredRoleValidations = {
   ...mandatoryValidations,
   role: validatePresence({
