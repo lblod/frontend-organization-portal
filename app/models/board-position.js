288070373709fbb7b035200463598e667b8ee6ee
Claire Lovisa <lovisa.claire@gmail.com>
2022-06-15 16:10:08 +0200

    Reorganize board and regular members

--- app/models/board-position.js
+++ app/models/board-position.js
@@ -10,6 +10,14 @@ export const BOARD_MEMBER_ROLES = [
   'f848fa3cc2c5fb7c581a116866293925', // Bestuurslid van het centraal bestuur van de eredienst
   '8c91c321ad477c4fc372ee36358d3ed4', // Expert van het centraal bestuur van de eredienst
   '6e26e94ea4b127eeb850fb6debe07271', // Vertegenwoordiger aangesteld door het representatief orgaan van het centraal bestuur van de eredienst
+  'fa0201b3-2cda-4821-a15e-350ff57d77e0', // Lid van college
+  '59a90e03-4f22-4bb9-8c91-132618db4b38', // Toegevoegde schepen
+  '5ab0e9b8a3b2ca7c5e000011', // Gemeenteraadslid
+  '5ab0e9b8a3b2ca7c5e000017', // Lid van het Vast Bureau
+  '5ab0e9b8a3b2ca7c5e000019', // Lid van het Bijzonder Comité voor de Sociale Dienst
+  '5ab0e9b8a3b2ca7c5e000015', // Lid van de Raad voor Maatschappelijk Welzijn
+  '5ab0e9b8a3b2ca7c5e00001b', // Districtsraadslid
+  '5ab0e9b8a3b2ca7c5e00001f', // Provincieraadslid
 ];
 
 export const MANDATARIES_ROLES = [
@@ -19,27 +27,18 @@ export const MANDATARIES_ROLES = [
   '5960262f753661cf84329f3afa9f7df7', // Voorzitter van het centraal bestuur van de eredienst
   'e2af0ea1a6af96cfb698ac39ad985eea', // Secretaris van het centraal bestuur van de eredienst
   '5ab0e9b8a3b2ca7c5e000013', // Burgemeester
-  '2aaad652-d2eb-4333-9e9d-ce2888037ba8', // Voorzitter van het college"
-  '7b038cc40bba10bec833ecfe6f15bc7a', // Aangewezen burgemeester"
-  'fa0201b3-2cda-4821-a15e-350ff57d77e0', // Lid van college"
-  '59a90e03-4f22-4bb9-8c91-132618db4b38', // Toegevoegde schepen"
-  '5ab0e9b8a3b2ca7c5e000012', // Voorzitter van de gemeenteraad"
-  '5ab0e9b8a3b2ca7c5e000011', // Gemeenteraadslid"
-  '5ab0e9b8a3b2ca7c5e00001a', // Voorzitter van het Bijzonder Comité voor de Sociale Dienst"
-  '5ab0e9b8a3b2ca7c5e000018', // Voorzitter van het Vast Bureau"
-  '5ab0e9b8a3b2ca7c5e000017', // Lid van het Vast Bureau"
-  '5ab0e9b8a3b2ca7c5e00001a', // Voorzitter van het Bijzonder Comité voor de Sociale Dienst"
-  '5ab0e9b8a3b2ca7c5e000019', // Lid van het Bijzonder Comité voor de Sociale Dienst"
-  '5ab0e9b8a3b2ca7c5e000016', // Voorzitter van de Raad voor Maatschappelijk Welzijn"
-  '5ab0e9b8a3b2ca7c5e000015', // Lid van de Raad voor Maatschappelijk Welzijn"
-  '5ab0e9b8a3b2ca7c5e00001d', // Districtsburgemeester"
-  '5ab0e9b8a3b2ca7c5e00001e', // Districtsschepen"
-  '5ab0e9b8a3b2ca7c5e00001b', // Districtsraadslid"
-  '5ab0e9b8a3b2ca7c5e00001c', // Voorzitter van de districtsraad"
-  '5ab0e9b8a3b2ca7c5e000020', // Gedeputeerde"
-  'd7c00cd1-baf1-4346-83c0-6796c0bedd85', // Gouverneur"
-  '5ab0e9b8a3b2ca7c5e000027', // Voorzitter van de provincieraad"
-  '5ab0e9b8a3b2ca7c5e00001f', // Provincieraadslid"
+  '7b038cc40bba10bec833ecfe6f15bc7a', // Aangewezen burgemeester
+  '5ab0e9b8a3b2ca7c5e000012', // Voorzitter van de gemeenteraad
+  '5ab0e9b8a3b2ca7c5e00001a', // Voorzitter van het Bijzonder Comité voor de Sociale Dienst
+  '5ab0e9b8a3b2ca7c5e000018', // Voorzitter van het Vast Bureau
+  '5ab0e9b8a3b2ca7c5e00001a', // Voorzitter van het Bijzonder Comité voor de Sociale Dienst
+  '5ab0e9b8a3b2ca7c5e000016', // Voorzitter van de Raad voor Maatschappelijk Welzijn
+  '5ab0e9b8a3b2ca7c5e00001d', // Districtsburgemeester
+  '5ab0e9b8a3b2ca7c5e00001e', // Districtsschepen
+  '5ab0e9b8a3b2ca7c5e00001c', // Voorzitter van de districtsraad
+  '5ab0e9b8a3b2ca7c5e000020', // Gedeputeerde
+  'd7c00cd1-baf1-4346-83c0-6796c0bedd85', // Gouverneur
+  '5ab0e9b8a3b2ca7c5e000027', // Voorzitter van de provincieraad
 ];
 
 export default class BoardPositionModel extends Model {
