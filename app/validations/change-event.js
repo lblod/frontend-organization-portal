eb5a4cfa39f829959120ebcb5b60dbef1131929a
Nordine Bittich <nordine@bittich.be>
2022-07-15 09:36:49 +0200

    fix date error translation

--- app/validations/change-event.js
+++ app/validations/change-event.js
@@ -1,9 +1,12 @@
 import { validatePresence } from 'ember-changeset-validations/validators';
 import { validateUrl } from 'frontend-organization-portal/validators/url';
 
+const REQUIRED_MESSAGE = 'Vul de datum in.';
+
 export const changeEventValidations = {
   date: validatePresence({
     presence: true,
+    message: REQUIRED_MESSAGE,
     ignoreBlank: true,
   }),
 };
