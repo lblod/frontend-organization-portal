92afd71499d37b0669447590d8cd8249015996c7
Nordine Bittich <nordine@bittich.be>
2022-08-31 17:52:19 +0200

    sub organization not required

--- app/validations/worship-administrative-unit.js
+++ app/validations/worship-administrative-unit.js
@@ -1,9 +1,9 @@
 import { isBlank } from '@ember/utils';
 import { validatePresence } from 'ember-changeset-validations/validators';
-import { validateConditionally } from 'frontend-organization-portal/validators/validate-conditionally';
+//import { validateConditionally } from 'frontend-organization-portal/validators/validate-conditionally';
 import { ID_NAME } from 'frontend-organization-portal/models/identifier';
-import { RECOGNIZED_WORSHIP_TYPE } from 'frontend-organization-portal/models/recognized-worship-type';
-import { CLASSIFICATION_CODE } from 'frontend-organization-portal/models/administrative-unit-classification-code';
+//import { RECOGNIZED_WORSHIP_TYPE } from 'frontend-organization-portal/models/recognized-worship-type';
+//import { CLASSIFICATION_CODE } from 'frontend-organization-portal/models/administrative-unit-classification-code';
 
 export default {
   name: validatePresence({ presence: true, ignoreBlank: true }),
@@ -27,37 +27,37 @@ export default {
     ignoreBlank: true,
     message: 'Selecteer een optie',
   }),
-  isSubOrganizationOf: validateConditionally(
-    validatePresence({
-      presence: true,
-      ignoreBlank: true,
-      message: 'Selecteer een optie',
-    }),
-    function (changes, content) {
-      let unit = null;
-      if (changes.classification?.id && changes.recognizedWorshipType?.id) {
-        unit = changes;
-      } else {
-        unit = content;
-      }
+  // isSubOrganizationOf: validateConditionally(
+  //   validatePresence({
+  //     presence: true,
+  //     ignoreBlank: true,
+  //     message: 'Selecteer een optie',
+  //   }),
+  //   function (changes, content) {
+  //     let unit = null;
+  //     if (changes.classification?.id && changes.recognizedWorshipType?.id) {
+  //       unit = changes;
+  //     } else {
+  //       unit = content;
+  //     }
 
-      const isWorshipService =
-        unit.classification?.get('id') === CLASSIFICATION_CODE.WORSHIP_SERVICE;
+  //     const isWorshipService =
+  //       unit.classification?.get('id') === CLASSIFICATION_CODE.WORSHIP_SERVICE;
 
-      const typesThatHaveACentralWorshipService = [
-        RECOGNIZED_WORSHIP_TYPE.ISLAMIC,
-        RECOGNIZED_WORSHIP_TYPE.ROMAN_CATHOLIC,
-        RECOGNIZED_WORSHIP_TYPE.ORTHODOX,
-      ];
+  //     const typesThatHaveACentralWorshipService = [
+  //       RECOGNIZED_WORSHIP_TYPE.ISLAMIC,
+  //       RECOGNIZED_WORSHIP_TYPE.ROMAN_CATHOLIC,
+  //       RECOGNIZED_WORSHIP_TYPE.ORTHODOX,
+  //     ];
 
-      return (
-        isWorshipService &&
-        typesThatHaveACentralWorshipService.find(
-          (id) => id == unit.recognizedWorshipType?.get('id')
-        )
-      );
-    }
-  ),
+  //     return (
+  //       isWorshipService &&
+  //       typesThatHaveACentralWorshipService.find(
+  //         (id) => id == unit.recognizedWorshipType?.get('id')
+  //       )
+  //     );
+  //   }
+  // ),
 };
 
 export function getStructuredIdentifierKBOValidations(store) {
