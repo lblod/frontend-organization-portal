26a9f86eca16e223d34fe9fa9ca434891fec711a
iurianu <iulianandriescu@gmail.com>
2022-09-29 12:20:22 +0300

    add banner for environment - OP-1632

--- app/components/environment-label.js
+++ app/components/environment-label.js
@@ -8,9 +8,18 @@ const ENVIRONMENT_NAME = {
 };
 
 const ENVIRONMENT_NAME_CLASS = {
-  [ENVIRONMENT_NAME.LOCAL]: 'au-c-environment-pill--local',
-  [ENVIRONMENT_NAME.DEV]: 'au-c-environment-pill--dev',
-  [ENVIRONMENT_NAME.QA]: 'au-c-environment-pill--qa',
+  [ENVIRONMENT_NAME.LOCAL]: 'au-c-environment--local',
+  [ENVIRONMENT_NAME.DEV]: 'au-c-environment--dev',
+  [ENVIRONMENT_NAME.QA]: 'au-c-environment--qa',
+};
+
+const ENVIRONMENT_NAME_TEXT = {
+  [ENVIRONMENT_NAME.LOCAL]:
+    'Dit is de lokale omgeving van het Organisatieportaal met fictieve en testgegevens. De productieomgeving met de echte data vind je op https://organisations.abb.vlaanderen.be',
+  [ENVIRONMENT_NAME.DEV]:
+    'Dit is de ontwikkelomgeving van het OrganisatiePortaal met fictieve en testgegevens. De productieomgeving met de echte data vind je op https://organisaties.abb.vlaanderen.be',
+  [ENVIRONMENT_NAME.QA]:
+    'Dit is de testomgeving</strong> van het OrganisatiePortaal met fictieve en testgegevens. De productieomgeving met de echte data vind je op https://organisaties.abb.vlaanderen.be',
 };
 
 export default class EnvironmentLabelComponent extends Component {
@@ -24,5 +33,9 @@ export default class EnvironmentLabelComponent extends Component {
 
   get environmentClass() {
     return ENVIRONMENT_NAME_CLASS[this.environmentName];
+  }
+
+  get environmentDescription() {
+    return ENVIRONMENT_NAME_TEXT[this.environmentName];
   }
 }
