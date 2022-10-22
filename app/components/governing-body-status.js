3a88ccff18ab23139a6400dd032058deafca9f58
Boris De Vloed <boris.de.vloed@gmail.com>
2022-10-22 10:51:35 +0200

    fix status logic

--- app/components/governing-body-status.js
+++ app/components/governing-body-status.js
@@ -4,12 +4,14 @@ export default class GoverningBodyStatusComponent extends Component {
   get isActive() {
     const today = new Date();
 
-    if (this.args.startDate) {
-      return !this.args.endDate || this.args.endDate > today;
-    } else if (this.args.endDate) {
-      return this.args.endDate > today;
-    } else {
-      return false;
-    }
+    const start_unknown = !this.args.startDate;
+    const start_in_past = this.args.startDate && this.args.startDate < today;
+    const end_unknown = !this.args.endDate;
+    const end_in_future = this.args.endDate && this.args.endDate > today;
+
+    return (
+      (start_in_past && (end_in_future || end_unknown)) ||
+      (start_unknown && end_in_future)
+    );
   }
 }
