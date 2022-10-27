ccb50ec22ab6ccb7f58331d170ead1ac7367baf9
Iulian Andriescu <iulianandriescu@gmail.com>
2022-10-27 18:44:19 +0300

    Fix / Setup different banner for local environment (#413)
    different banner for local environment

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,15 +1,29 @@
 import Controller from '@ember/controller';
 import { getOwner } from '@ember/application';
 
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]'
+);
+
 export default class ApplicationController extends Controller {
   get environmentName() {
-    return getOwner(this).resolveRegistration('config:environment')
-      .environmentName;
+    const thisEnvironmentValues = isLocalhost
+      ? 'local'
+      : getOwner(this).resolveRegistration('config:environment')
+          .environmentName;
+
+    return thisEnvironmentValues;
   }
 
   get environmentTitle() {
-    return getOwner(this).resolveRegistration('config:environment')
-      .environmentTitle;
+    const thisEnvironmentValues = isLocalhost
+      ? 'lokalomgeving'
+      : getOwner(this).resolveRegistration('config:environment')
+          .environmentTitle;
+
+    return thisEnvironmentValues;
   }
 
   get showEnvironment() {
