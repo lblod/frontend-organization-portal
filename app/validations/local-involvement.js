51bba6d65b357634832e9022e8c53e83a048766a
Nordine Bittich <3816305+nbittich@users.noreply.github.com>
2023-03-09 16:49:06 +0100

    validate involvement type (#453)

--- app/validations/local-involvement.js
+++ app/validations/local-involvement.js
@@ -5,6 +5,7 @@ import {
 } from 'ember-changeset-validations/validators';
 import { validateConditionally } from 'frontend-organization-portal/validators/validate-conditionally';
 import { INVOLVEMENT_TYPE } from 'frontend-organization-portal/models/involvement-type';
+import { CLASSIFICATION_CODE } from 'frontend-organization-portal/models/administrative-unit-classification-code';
 
 const PERCENTAGE_ERROR_MESSAGE = {
   ['notANumber']: 'Vul het percentage in',
@@ -18,11 +19,14 @@ export default {
     ignoreBlank: true,
     message: 'Selecteer een lokaal bestuur',
   }),
-  involvementType: validatePresence({
-    presence: true,
-    ignoreBlank: true,
-    message: 'Selecteer een type betrokkenheid',
-  }),
+  involvementType: [
+    validatePresence({
+      presence: true,
+      ignoreBlank: true,
+      message: 'Selecteer een type betrokkenheid',
+    }),
+    validateConditionally(validateInvolvementType, isProvince),
+  ],
   percentage: validateConditionally(
     validateNumber({
       gt: 0,
@@ -58,4 +62,38 @@ async function isFinancialInvolvementType(changes, content) {
 
 function isInvolvementTypeRecord(maybeRecord) {
   return maybeRecord?.constructor?.modelName === 'involvement-type';
+}
+async function isProvince(changes, content) {
+  let unit = await changes.administrativeUnit;
+
+  if (!unit?.classification) {
+    unit = await content.administrativeUnit;
+  }
+
+  const classification = await unit.classification;
+  if (classification) {
+    return classification.id === CLASSIFICATION_CODE.PROVINCE;
+  } else {
+    return false;
+  }
+}
+
+async function validateInvolvementType(
+  key,
+  newValue,
+  oldValue,
+  changes,
+  content
+) {
+  const ADVISEREND_TYPE = '0f845f00ee76099c89518cbaf6a7b77f';
+  let involvementType = await changes.involvementType;
+
+  if (!isInvolvementTypeRecord(involvementType)) {
+    involvementType = await content.involvementType;
+  }
+  if (involvementType.id === ADVISEREND_TYPE) {
+    return 'Adviserend is geen geldige keuze voor een provincie';
+  }
+
+  return true;
 }
