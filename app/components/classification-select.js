d0b4bddaeaab895c1186091419fad94aa5b652e3
Nordine Bittich <nordine@bittich.be>
2023-04-06 14:02:15 +0200

    add classification

--- app/components/classification-select.js
+++ app/components/classification-select.js
@@ -58,6 +58,8 @@ export default class ClassificationSelectComponent extends Component {
         CLASSIFICATION_CODE.PROVINCE,
         CLASSIFICATION_CODE.OCMW,
         CLASSIFICATION_CODE.DISTRICT,
+        CLASSIFICATION_CODE.AGB,
+        CLASSIFICATION_CODE.APB,
       ];
     }
 
