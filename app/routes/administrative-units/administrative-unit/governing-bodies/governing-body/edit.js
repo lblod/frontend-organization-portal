29012bef9de2076c79f0c66faf77f0c6ad945236
Nordine Bittich <nordine@bittich.be>
2023-05-15 15:00:51 +0200

    validate governing bodies date

--- app/routes/administrative-units/administrative-unit/governing-bodies/governing-body/edit.js
+++ app/routes/administrative-units/administrative-unit/governing-bodies/governing-body/edit.js
@@ -1,5 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { createValidatedChangeset } from 'frontend-organization-portal/utils/changeset';
+import governingBodyValidations from 'frontend-organization-portal/validations/governing-body';
 
 export default class AdministrativeUnitsAdministrativeUnitGoverningBodiesGoverningBodyEditRoute extends Route {
   @service currentSession;
@@ -13,9 +15,28 @@ export default class AdministrativeUnitsAdministrativeUnitGoverningBodiesGoverni
     }
   }
 
-  model() {
-    return this.modelFor(
-      'administrative-units.administrative-unit.governing-bodies.governing-body'
+  async model() {
+    const { administrativeUnit, governingBodyClassification, governingBody } =
+      await this.modelFor(
+        'administrative-units.administrative-unit.governing-bodies.governing-body'
+      );
+
+    const bodies = await this.modelFor(
+      'administrative-units.administrative-unit.governing-bodies'
     );
+
+    const otherBodies = bodies.governingBodies.filter(
+      (otherBody) => otherBody.id !== governingBody.id
+    );
+
+    const bodyChangeSet = await createValidatedChangeset(
+      governingBody,
+      governingBodyValidations(otherBodies)
+    );
+    return {
+      governingBody: bodyChangeSet,
+      governingBodyClassification,
+      administrativeUnit,
+    };
   }
 }
---
+++ app/validations/governing-body.js
@@ -0,0 +1,85 @@
+import { validatePresence } from 'ember-changeset-validations/validators';
+
+const governingBodyValidations = (governingBodies) => {
+  return {
+    startDate: [
+      validatePresence({
+        presence: true,
+        ignoreBlank: true,
+        message: 'Vul de startdatum in',
+      }),
+      validateStartDateBeforeEndDate(governingBodies),
+    ],
+    endDate: [
+      validatePresence({
+        presence: true,
+        ignoreBlank: true,
+        message: 'Vul de effectieve einddatum in',
+      }),
+      validateEndDateAfterStartDate(governingBodies),
+    ],
+  };
+};
+
+export default governingBodyValidations;
+
+function validateEndDateAfterStartDate(governingBodies) {
+  return (_key, newValue, oldValue, changes, content) => {
+    const endDateValue = newValue || oldValue;
+    const endDate = new Date(endDateValue);
+    const newStartDateValue = changes?.startDate || content.startDate;
+    if (newStartDateValue) {
+      const newStartDate = new Date(newStartDateValue);
+      if (newStartDate.getTime() > endDate.getTime()) {
+        return 'Kies een einddatum die na de startdatum plaatsvindt';
+      }
+      return validateNoOverlap(governingBodies, newStartDate, endDate);
+    }
+    return true;
+  };
+}
+function validateStartDateBeforeEndDate(governingBodies) {
+  return (_key, newValue, _oldValue, changes, content) => {
+    // The messages should also appear when the content of one of the fields is deleted.
+    const effectiveEndDateValue = changes?.endDate || content.endDate;
+
+    const newStartDate = new Date(newValue);
+
+    if (effectiveEndDateValue) {
+      const effectiveEndDate = new Date(effectiveEndDateValue);
+      if (newStartDate.getTime() > effectiveEndDate.getTime()) {
+        return 'Kies een startdatum die vóór de einddatum plaatsvindt';
+      }
+      return validateNoOverlap(governingBodies, newStartDate, effectiveEndDate);
+    }
+
+    return true;
+  };
+}
+
+function validateNoOverlap(otherBodies, startDate, endDate) {
+  for (const body of otherBodies) {
+    if (body.startDate) {
+      const otherStartDate = new Date(body.startDate).getTime();
+
+      if (
+        otherStartDate >= startDate.getTime() &&
+        otherStartDate <= endDate.getTime()
+      ) {
+        return 'Geen overlap';
+      }
+    }
+    if (body.endDate) {
+      const otherEndDate = new Date(body.endDate).getTime();
+
+      if (
+        otherEndDate >= startDate.getTime() &&
+        otherEndDate <= endDate.getTime()
+      ) {
+        return 'Geen overlap';
+      }
+    }
+  }
+
+  return true;
+}
