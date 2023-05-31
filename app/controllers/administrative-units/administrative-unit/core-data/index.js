efc807bff5de5df07e48cfe76e6f405145bcbcbd
Nordine Bittich <nordine@bittich.be>
2023-05-31 15:27:26 +0200

    show sp identificator as clickable in frontend

--- app/controllers/administrative-units/administrative-unit/core-data/index.js
+++ app/controllers/administrative-units/administrative-unit/core-data/index.js
@@ -8,6 +8,15 @@ const SHAREPOINT_LINK_BASE = {
     'https://vlaamseoverheid.sharepoint.com/sites/Abb-Erediensten/Lists/Lijst%20Eredienstbesturen/AllItems.aspx?view=7&q=',
   CENTRAL_WORSHIP_SERVICE:
     'https://vlaamseoverheid.sharepoint.com/sites/Abb-Erediensten/Lists/Lijst%20CKB1/AllItems.aspx?view=7&q=',
+  MUNICIPALITY:
+    'https://vlaamseoverheid.sharepoint.com/sites/Abb-LokFin/Lists/Organisaties/DispForm.aspx?ID=',
+  DISTRICT:
+    'https://vlaamseoverheid.sharepoint.com/sites/Abb-LokFin/Lists/Organisaties/DispForm.aspx?ID=',
+  PROVINCE:
+    'https://vlaamseoverheid.sharepoint.com/sites/Abb-LokFin/Lists/Organisaties/DispForm.aspx?ID=',
+  OCMW: 'https://vlaamseoverheid.sharepoint.com/sites/Abb-LokFin/Lists/Organisaties/DispForm.aspx?ID=',
+  AGB: 'https://vlaamseoverheid.sharepoint.com/sites/Abb-LokFin/Lists/Organisaties/DispForm.aspx?ID=',
+  APB: 'https://vlaamseoverheid.sharepoint.com/sites/Abb-LokFin/Lists/Organisaties/DispForm.aspx?ID=',
 };
 
 export default class AdministrativeUnitsAdministrativeUnitCoreDataIndexController extends Controller {
@@ -62,9 +71,56 @@ export default class AdministrativeUnitsAdministrativeUnitCoreDataIndexControlle
     );
   }
 
+  get isProvince() {
+    return (
+      this.model.administrativeUnit.classification?.get('id') ===
+      CLASSIFICATION_CODE.PROVINCE
+    );
+  }
+
+  get isDistrict() {
+    return (
+      this.model.administrativeUnit.classification?.get('id') ===
+      CLASSIFICATION_CODE.DISTRICT
+    );
+  }
+
+  get isOCMW() {
+    return (
+      this.model.administrativeUnit.classification?.get('id') ===
+      CLASSIFICATION_CODE.OCMW
+    );
+  }
+
+  get isAgb() {
+    return (
+      this.model.administrativeUnit.classification?.get('id') ===
+      CLASSIFICATION_CODE.AGB
+    );
+  }
+  get isApb() {
+    return (
+      this.model.administrativeUnit.classification?.get('id') ===
+      CLASSIFICATION_CODE.APB
+    );
+  }
+
   get sharePointLinkBase() {
-    return this.isWorshipService
-      ? SHAREPOINT_LINK_BASE.WORSHIP_SERVICE
-      : SHAREPOINT_LINK_BASE.CENTRAL_WORSHIP_SERVICE;
+    if (this.isWorshipService) {
+      return SHAREPOINT_LINK_BASE.WORSHIP_SERVICE;
+    } else if (this.isDistrict) {
+      return SHAREPOINT_LINK_BASE.DISTRICT;
+    } else if (this.isProvince) {
+      return SHAREPOINT_LINK_BASE.PROVINCE;
+    } else if (this.isMunicipality) {
+      return SHAREPOINT_LINK_BASE.MUNICIPALITY;
+    } else if (this.isOCMW) {
+      return SHAREPOINT_LINK_BASE.OCMW;
+    } else if (this.isAgb) {
+      return SHAREPOINT_LINK_BASE.AGB;
+    } else if (this.isApb) {
+      return SHAREPOINT_LINK_BASE.APB;
+    }
+    return SHAREPOINT_LINK_BASE.CENTRAL_WORSHIP_SERVICE;
   }
 }
