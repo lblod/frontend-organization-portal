fe11d6b500abcd33399308af6f5b214c0807483b
Nordine Bittich <nordine@bittich.be>
2023-06-01 08:59:07 +0200

    also handle other error status

--- app/routes/organizations.js
+++ app/routes/organizations.js
@@ -16,15 +16,8 @@ export default class OrganizationsRoute extends Route {
   @action
   error(error) {
     console.log(error);
-    if (
-      error?.errors?.map((e) => e.status).find((status) => status === '404')
-    ) {
-      this.router.transitionTo('route-not-found', {
-        wildcard: 'pagina-niet-gevonden',
-      });
-    } else {
-      // Let the route above this handle the error.
-      return true;
-    }
+    this.router.transitionTo('route-not-found', {
+      wildcard: 'pagina-niet-gevonden',
+    });
   }
 }
