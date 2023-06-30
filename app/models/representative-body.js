6a49082453728f3e6314a968ebb7c18e011af206
Claire Lovisa <claire-lovisa@users.noreply.github.com>
2023-06-30 11:49:36 +0200

    Blacklist a RO from dropdown list (#473)

--- app/models/representative-body.js
+++ app/models/representative-body.js
@@ -1,6 +1,10 @@
 import { belongsTo, hasMany } from '@ember-data/model';
 import OrganizationModel from './organization';
 
+export const BLACKLIST_RO = [
+  'e224c637ba8bb0e5dfbb87da225b4652', // Executief van de Moslims van België
+];
+
 export default class RepresentativeBodyModel extends OrganizationModel {
   @belongsTo('recognized-worship-type', {
     inverse: null,
