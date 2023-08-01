fe7ad00f2348f8d8b687c5c2c3d59fc681616565
Oscar <lagartoverde97@gmail.com>
2023-08-01 10:14:25 +0200

    first step upgrading

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function (environment) {
-  let ENV = {
+  const ENV = {
     modulePrefix: 'frontend-organization-portal',
     environment,
     rootURL: '/',
@@ -11,11 +11,11 @@ module.exports = function (environment) {
       extraPrefixes: '{{YASGUI_EXTRA_PREFIXES}}',
     },
     EmberENV: {
+      EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
       },
-      EXTEND_PROTOTYPES: false,
     },
 
     APP: {
