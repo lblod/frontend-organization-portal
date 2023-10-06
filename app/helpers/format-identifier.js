b481ebc280848d009ff4dd4e8b5644506cf5c3f8
Alexander van den Berghe <alexander.van.den.berghe@redpencil.io>
2023-10-06 19:33:19 +0200

    Fix: use only alphanumeric characters to match identifiers

--- app/helpers/format-identifier.js
+++ app/helpers/format-identifier.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export function formatIdentifier([identifier]) {
-  return identifier.replace(/[^\w]/gi, '');
+  return identifier.replace(/[^a-zA-Z0-9]/gi, '');
 }
 
 export default helper(formatIdentifier);
