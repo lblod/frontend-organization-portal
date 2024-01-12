776ba7ed7790e53c05dd6a8e7aaad350765a0c33
mirdono <alexvdberghe@fastmail.com>
2024-01-12 10:54:40 +0100

    Initial validation schema

--- app/models/governing-body.js
+++ app/models/governing-body.js
@@ -1,6 +1,12 @@
-import Model, { attr, hasMany, belongsTo } from '@ember-data/model';
+import { attr, hasMany, belongsTo } from '@ember-data/model';
+import AbstractValidationModel from './abstract-validation-model';
+import Joi from 'joi';
+import {
+  validateBelongsToOptional,
+  validateHasManyOptional,
+} from '../validators/schema';
 
-export default class GoverningBodyModel extends Model {
+export default class GoverningBodyModel extends AbstractValidationModel {
   @attr('date') startDate;
   @attr('date') endDate;
 
@@ -45,5 +51,40 @@ export default class GoverningBodyModel extends Model {
       period = '- ' + this.endDate.getFullYear();
     }
     return period;
+  }
+
+  get validationSchema() {
+    return Joi.object({
+      startDate: Joi.date()
+        .empty(null)
+        .required()
+        .when(Joi.ref('endDate'), {
+          is: Joi.exist(),
+          then: Joi.date().max(Joi.ref('endDate')),
+        })
+        .messages({
+          'any.required': 'Vul de startdatum in',
+          'date.max': 'Kies een startdatum die vóór de einddatum plaatsvindt',
+        }),
+      endDate: Joi.date()
+        .empty(null)
+        .required()
+        // TODO: results in an undefined model, cannot have cyclic refs?
+        // .when(Joi.ref('startDate'), {
+        //   is: Joi.date(),
+        //   then: Joi.date().min(Joi.ref('startDate')),
+        // })
+        .messages({
+          'any.required': 'Vul de einddatum in',
+          'date.min': 'Kies een einddatum die na de startdatum plaatsvindt',
+        }),
+      // TODO: validate there is no overlap with other governing bodies
+      administrativeUnit: validateBelongsToOptional(),
+      classification: validateBelongsToOptional(),
+      isTimeSpecializationOf: validateBelongsToOptional(),
+      hasTimeSpecializations: validateHasManyOptional(),
+      mandates: validateHasManyOptional(),
+      boardPositions: validateHasManyOptional(),
+    });
   }
 }
