c3f741cedd56c4d25b8ed006de9c6808cfec9ba0
mirdono <alexvdberghe@fastmail.com>
2024-01-12 17:20:15 +0100

    Added Joi validation for decisions

--- app/models/decision.js
+++ app/models/decision.js
@@ -1,6 +1,9 @@
-import Model, { attr, belongsTo } from '@ember-data/model';
+import { attr, belongsTo } from '@ember-data/model';
+import AbstractValidationModel from './abstract-validation-model';
+import Joi from 'joi';
+import { validateBelongsToOptional, validateUrl } from '../validators/schema';
 
-export default class DecisionModel extends Model {
+export default class DecisionModel extends AbstractValidationModel {
   @attr('date') publicationDate;
   @attr documentLink;
 
@@ -8,6 +11,15 @@ export default class DecisionModel extends Model {
     inverse: 'givesCauseTo',
   })
   hasDecisionActivity;
+
+  get validationSchema() {
+    return Joi.object({
+      // TODO: is the date really optional?
+      publicationDate: Joi.date().allow(null),
+      documentLink: validateUrl('Geef een geldig internetadres in'),
+      hasDecisionActivity: validateBelongsToOptional(),
+    });
+  }
 }
 
 export function isEmpty(decisionRecord) {
