6eba4e509aaa835164db28196a76c67c314a869c
mirdono <alexvdberghe@fastmail.com>
2024-01-12 17:35:22 +0100

    Converted DecisionActivity to Joi validation

--- app/models/decision-activity.js
+++ app/models/decision-activity.js
@@ -1,10 +1,20 @@
-import Model, { attr, hasMany } from '@ember-data/model';
+import { attr, hasMany } from '@ember-data/model';
+import AbstractValidationModel from './abstract-validation-model';
+import Joi from 'joi';
+import { validateHasManyOptional } from '../validators/schema';
 
-export default class DecisionActivityModel extends Model {
+export default class DecisionActivityModel extends AbstractValidationModel {
   @attr('date') endDate;
 
   @hasMany('decision', {
     inverse: 'hasDecisionActivity',
   })
   givesCauseTo;
+
+  get validationSchema() {
+    return Joi.object({
+      endDate: Joi.date().allow(null),
+      givesCauseTo: validateHasManyOptional(),
+    });
+  }
 }
