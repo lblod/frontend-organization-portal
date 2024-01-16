adf94f3d730f7528f1569e506c41650ba374b87a
mirdono <alexvdberghe@fastmail.com>
2024-01-16 15:24:49 +0100

    Converted validations for change events to Joi

--- app/models/change-event.js
+++ app/models/change-event.js
@@ -1,5 +1,15 @@
-import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
-export default class ChangeEventModel extends Model {
+import { attr, belongsTo, hasMany } from '@ember-data/model';
+import AbstractValidationModel from './abstract-validation-model';
+import Joi from 'joi';
+import {
+  validateBelongsToOptional,
+  validateBelongsToRequired,
+  validateHasManyOptional,
+  validateHasManyRequired,
+} from '../validators/schema';
+import { CHANGE_EVENT_TYPE } from './change-event-type';
+
+export default class ChangeEventModel extends AbstractValidationModel {
   @attr('date') date;
   @attr description;
 
@@ -27,4 +37,36 @@ export default class ChangeEventModel extends Model {
     inverse: 'resultFrom',
   })
   results;
+
+  get validationSchema() {
+    return Joi.object({
+      date: Joi.date()
+        .empty(null)
+        .required()
+        .messages({ 'any.required': 'Vul de datum in' }),
+      description: Joi.string().empty(''),
+      type: validateBelongsToRequired('Selecteer een type'),
+      decision: validateBelongsToOptional(),
+      resultingOrganizations: Joi.when('type.id', {
+        is: Joi.exist().valid(
+          CHANGE_EVENT_TYPE.FUSIE,
+          CHANGE_EVENT_TYPE.MERGER
+        ),
+        then: validateHasManyRequired('Selecteer een resulterende organisatie'),
+        otherwise: validateHasManyOptional(),
+      }),
+      originalOrganizations: Joi.when('type.id', {
+        is: Joi.exist().valid(
+          CHANGE_EVENT_TYPE.FUSIE,
+          CHANGE_EVENT_TYPE.MERGER
+        ),
+        then: Joi.array()
+          .required()
+          .min(2)
+          .messages({ '*': 'Selecteer een betrokken organisatie' }),
+        otherwise: validateHasManyOptional(),
+      }),
+      results: validateHasManyOptional(),
+    });
+  }
 }
