1a818ff453c0ba8a8c271b98e94e4883e20ecb92
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2024-01-16 12:49:31 +0100

    Remove disabled `person`  and `minister` edit and new forms
    Remove route, controller and template forms:
    - `people.person.positions.minister.edit`
    - `people.person.personal-information.edit`
    - `administrative-units.administrative-unit.ministers.new`
    Remove form code related of `people.new-position`

--- app/controllers/people/person/positions/minister/index.js
+++ app/controllers/people/person/positions/minister/index.js
@@ -1,7 +1,3 @@
 import Controller from '@ember/controller';
 
-export default class MinisterIndexController extends Controller {
-  get positionsCantBeCreatedOrEdited() {
-    return new Date() >= new Date('2023-02-01');
-  }
-}
+export default class MinisterIndexController extends Controller {}
