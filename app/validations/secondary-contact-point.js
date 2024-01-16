1c8d73be424184cea03b819d248572e114b93cde
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2024-01-16 14:03:57 +0100

    Delete unused address, contact-point, secondary-contact-point, and site validations

--- app/validations/secondary-contact-point.js
+++
@@ -1,9 +0,0 @@
-import { validateFormat } from 'ember-changeset-validations/validators';
-
-export default {
-  telephone: validateFormat({
-    allowBlank: true,
-    regex: /^(tel:)?\+?[0-9]*$/,
-    message: 'Enkel een plusteken en cijfers zijn toegelaten',
-  }),
-};
