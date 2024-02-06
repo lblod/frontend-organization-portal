9e94e8da7949aafa8805782244549b8f1a11d28c
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2024-02-06 10:37:53 +0100

    chore: Remove `ember-changeset` and all remaining traces

--- app/utils/changeset.js
+++
@@ -1,8 +0,0 @@
-import Changeset from 'ember-changeset';
-import lookupValidator from 'ember-changeset-validations';
-
-export function createValidatedChangeset(obj, validations) {
-  return new Changeset(obj, lookupValidator(validations), validations, {
-    skipValidate: true,
-  });
-}
