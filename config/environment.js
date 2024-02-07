81dc600095444b049322eb2a901f6e3655deedf5
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2024-02-07 14:44:20 +0100

    chore: v3.28.5...v4.4.1

--- config/environment.js
+++ config/environment.js
@@ -6,10 +6,6 @@ module.exports = function (environment) {
     environment,
     rootURL: '/',
     locationType: 'history',
-    yasgui: {
-      defaultQuery: '{{YASGUI_DEFAULT_QUERY}}',
-      extraPrefixes: '{{YASGUI_EXTRA_PREFIXES}}',
-    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -70,6 +66,11 @@ module.exports = function (environment) {
 
     features: {
       'kbo-data-tab': false,
+    },
+
+    yasgui: {
+      defaultQuery: '{{YASGUI_DEFAULT_QUERY}}',
+      extraPrefixes: '{{YASGUI_EXTRA_PREFIXES}}',
     },
   };
 
