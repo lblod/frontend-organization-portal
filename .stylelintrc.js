f4f02b8a4270085342d97af968253316071b9181
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2024-02-15 17:26:49 +0100

    chore: Enable SCSS linter + run lint:fix

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  extends: ['stylelint-config-standard-scss', 'stylelint-prettier/recommended'],
+  rules: {
+    'selector-class-pattern': null, // This enforces kebab-case but we use BEM which isn't compatible with this rule
+    'no-descending-specificity': null, // To be removed once we have fixed all the issues
+    'scss/at-extend-no-missing-placeholder': null, // To be removed once we have fixed all the issues
+  },
 };
