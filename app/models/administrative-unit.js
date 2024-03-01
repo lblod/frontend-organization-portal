f1d3bfd5991c8744ec2a80d9564c5d036cdd370e
mirdono <alexvdberghe@fastmail.com>
2024-03-01 10:35:13 +0100

    chore: added comment with background on relaxMandatoryFoundingOrganization option

--- app/models/administrative-unit.js
+++ app/models/administrative-unit.js
@@ -77,6 +77,15 @@ export default class AdministrativeUnitModel extends OrganizationModel {
         REQUIRED_MESSAGE
       ),
       wasFoundedByOrganizations: Joi.when(
+        // Note: For OCMW associations and PEVAs a founding organisation is
+        // normally mandatory. But the available business data when onboarding
+        // them was incomplete in this respect. Therefore, we opted to relax
+        // this rule for the OCMW associations and PEVAs imported during the
+        // onboarding. The `relaxMandatoryFoundingOrganization` option allows us
+        // to specify that a founding organisation is not mandatory in, for
+        // example, the edit core data form. Otherwise, the form validation
+        // would prevent editing the core data of the imported organisations due
+        // to the lack of founding organisation.
         Joi.ref('$relaxMandatoryFoundingOrganization'),
         {
           is: Joi.exist().valid(true),
