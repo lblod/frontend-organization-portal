50f9ded6012589fa887b50de3cba273f00a9f3b1
mirdono <alexander.van.den.berghe@redpencil.io>
2024-04-09 14:54:52 +0200

    feat: display organisations on index of organizations route

---
+++ app/controllers/organizations/index.js
@@ -0,0 +1,166 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { CLASSIFICATION_CODE } from 'frontend-organization-portal/models/administrative-unit-classification-code';
+
+export default class OrganizationsIndexController extends Controller {
+  @service router;
+  queryParams = [
+    'page',
+    'size',
+    'sort',
+    'name',
+    'identifier',
+    'municipality',
+    'province',
+    'classificationIds',
+    'recognizedWorshipTypeId',
+    'organizationStatus',
+  ];
+
+  @tracked page = 0;
+  size = 25;
+  @tracked sort = 'name';
+  @tracked name = '';
+  @tracked identifier = '';
+  @tracked municipality = '';
+  @tracked province = '';
+  @tracked classificationIds = '';
+  @tracked recognizedWorshipTypeId = '';
+  @tracked organizationStatus = '';
+
+  get organizations() {
+    return this.model.loadOrganizationsTaskInstance.isFinished
+      ? this.model.loadOrganizationsTaskInstance.value
+      : this.model.loadedOrganizations;
+  }
+
+  get isLoading() {
+    return this.model.loadOrganizationsTaskInstance.isRunning;
+  }
+
+  get hasPreviousData() {
+    return (
+      this.model.loadedOrganizations &&
+      this.model.loadedOrganizations.length > 0
+    );
+  }
+
+  get showTableLoader() {
+    return this.isLoading && !this.hasPreviousData;
+  }
+
+  get hasNoResults() {
+    return (
+      this.model.loadOrganizationsTaskInstance.isFinished &&
+      this.organizations.length === 0
+    );
+  }
+
+  get hasErrored() {
+    return this.model.loadOrganizationsTaskInstance.isError;
+  }
+
+  get modelHasOnlyWorshipOrganizations() {
+    if (this.organizations && this.organizations.length) {
+      return !this.organizations.slice().some((org) => {
+        return (
+          org.classification_id !== CLASSIFICATION_CODE.WORSHIP_SERVICE &&
+          org.classification_id !==
+            CLASSIFICATION_CODE.CENTRAL_WORSHIP_SERVICE &&
+          org.classification_id !== CLASSIFICATION_CODE.REPRESENTATIVE_ORGAN
+        );
+      });
+    }
+    return false;
+  }
+
+  @action
+  search(event) {
+    event.preventDefault();
+
+    if (this.page > 0) {
+      // resetting the pagination will refresh the model
+      this.resetPagination();
+    } else {
+      this.router.refresh();
+    }
+  }
+
+  @action
+  setClassificationIds(selection) {
+    this.page = null;
+
+    this.classificationIds = selection
+      .map((classification) => classification.id)
+      .join(',');
+  }
+
+  @action
+  setRecognizedWorshipTypeId(selection) {
+    this.page = null;
+    this.recognizedWorshipTypeId = selection?.id;
+  }
+
+  @action
+  setName(selection) {
+    this.page = null;
+    this.name = selection;
+  }
+
+  @action
+  setIdentifier(selection) {
+    this.page = null;
+    this.identifier = selection;
+  }
+
+  @action
+  setOrganizationStatus(selection) {
+    this.page = null;
+    if (selection !== null) {
+      this.organizationStatus = selection.id;
+    } else {
+      this.organizationStatus = '';
+    }
+  }
+
+  @action
+  setMunicipality(selection) {
+    this.page = null;
+    this.municipality = selection;
+  }
+
+  @action
+  setProvince(selection) {
+    // Don't reset pagination if the provincie is set automatically via municipality
+    if (!this.municipality) {
+      this.page = null;
+    }
+    if (selection !== null) {
+      this.province = selection;
+    } else {
+      this.province = '';
+    }
+  }
+
+  resetPagination() {
+    this.page = 0;
+  }
+
+  @action
+  resetFilters() {
+    this.name = '';
+    this.identifier = '';
+    this.municipality = '';
+    this.province = '';
+    this.classificationIds = '';
+    this.recognizedWorshipTypeId = '';
+    this.organizationStatus = '';
+    this.page = 0;
+    this.sort = 'name';
+
+    // Triggers a refresh of the model
+    this.page = null;
+  }
+}
--- app/routes/organizations/index.js
+++ app/routes/organizations/index.js
@@ -1,3 +1,99 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { keepLatestTask } from 'ember-concurrency';
+import { selectByRole as getClassificationIds } from 'frontend-organization-portal/utils/classification-identifiers';
+import { isNonAdministrativeUnit } from 'frontend-organization-portal/models/administrative-unit-classification-code';
 
-export default class OrganizationsIndexRoute extends Route {}
+export default class OrganizationsIndexRoute extends Route {
+  @service muSearch;
+  @service currentSession;
+
+  queryParams = {
+    page: { refreshModel: true },
+    sort: { refreshModel: true },
+    name: { refreshModel: true, replace: true },
+    identifier: { refreshModel: true, replace: true },
+    municipality: { refreshModel: true, replace: true },
+    province: { refreshModel: true, replace: true },
+    classificationIds: { refreshModel: true, replace: true },
+    recognizedWorshipTypeId: { refreshModel: true, replace: true },
+    organizationStatus: { refreshModel: true, replace: true },
+  };
+
+  async model(params) {
+    return {
+      loadOrganizationsTaskInstance: this.loadOrganizationsTask.perform(params),
+      loadedOrganizations: this.loadOrganizationsTask.lastSuccessful?.value,
+    };
+  }
+
+  @keepLatestTask({ cancelOn: 'deactivate' })
+  *loadOrganizationsTask(params) {
+    const filter = {};
+    if (params.name) {
+      let filterType = 'phrase_prefix';
+      let name = params.name.trim();
+
+      filter[`:${filterType}:legal_name,alternative_name,name`] = name;
+    }
+
+    if (params.identifier) {
+      // Notes:
+      // - toLowerCase is needed to properly match OVO numbers
+      // - use index field that only contains alphanumeric characters
+      //   (cf. mu-search configuration)
+      filter[':prefix:identifier.index'] = params.identifier
+        .toLowerCase()
+        .trim();
+    }
+
+    if (params.classificationIds) {
+      const queryIds = params.classificationIds
+        .split(',')
+        .map((id) => `(classification_id:${id})`)
+        .join(' OR ');
+
+      filter[':query:classification_id'] = queryIds;
+    } else {
+      filter['classification_id'] = getClassificationIds(
+        this.currentSession.hasWorshipRole
+      );
+    }
+
+    if (params.municipality) {
+      filter[':phrase:municipality'] = params.municipality;
+    }
+
+    if (params.province) {
+      filter[':phrase:province'] = params.province;
+    }
+
+    if (params.recognizedWorshipTypeId) {
+      filter['recognized_worship_type_id'] = params.recognizedWorshipTypeId;
+    }
+
+    if (params.organizationStatus) {
+      filter['status_id'] = params.organizationStatus;
+    } else {
+      filter[':query:status_id'] = `(_exists_:status_id)`;
+    }
+
+    return yield this.muSearch.search({
+      index: 'units',
+      page: params.page,
+      size: params.size,
+      sort: params.sort,
+      filters: filter,
+      dataMapping: (data) => {
+        const entry = data.attributes;
+        entry.id = data.id;
+        if (isNonAdministrativeUnit(entry.classification_id)) {
+          entry.route = 'organizations.organization.index';
+        } else {
+          entry.route = 'administrative-units.administrative-unit.index';
+        }
+        return entry;
+      },
+    });
+  }
+}
