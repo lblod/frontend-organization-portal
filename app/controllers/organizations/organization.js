0b12ddcbf685015e7242f131e15c725be6003e22
mirdono <alexander.van.den.berghe@redpencil.io>
2024-04-19 17:17:32 +0200

    refactor: use is* functions from model

--- app/controllers/organizations/organization.js
+++ app/controllers/organizations/organization.js
@@ -1,92 +1,15 @@
 import Controller from '@ember/controller';
-import {
-  CLASSIFICATION_CODE,
-  OCMW_ASSOCIATION_CLASSIFICATION_CODES,
-} from 'frontend-organization-portal/models/administrative-unit-classification-code';
 
 export default class OrganizationsOrganizationController extends Controller {
-  // TODO: remove and replace uses by model functions
-  get isWorshipOrganization() {
-    return this.isWorshipService || this.isCentralWorshipService;
-  }
-
-  get isWorshipService() {
-    return (
-      this.model.classification?.get('id') ===
-      CLASSIFICATION_CODE.WORSHIP_SERVICE
-    );
-  }
-
-  get isCentralWorshipService() {
-    return (
-      this.model.classification?.get('id') ===
-      CLASSIFICATION_CODE.CENTRAL_WORSHIP_SERVICE
-    );
-  }
-
-  get isProvince() {
-    return (
-      this.model.classification?.get('id') === CLASSIFICATION_CODE.PROVINCE
-    );
-  }
-
-  get isDistrict() {
-    return (
-      this.model.classification?.get('id') === CLASSIFICATION_CODE.DISTRICT
-    );
-  }
-
-  get isAgbOrApb() {
-    return this.isAgb || this.isApb;
-  }
-
-  get isAgb() {
-    return this.model.classification?.get('id') === CLASSIFICATION_CODE.AGB;
-  }
-
-  get isApb() {
-    return this.model.classification?.get('id') === CLASSIFICATION_CODE.APB;
-  }
-
-  get isIgs() {
-    return (
-      this.model.classification?.get('id') ===
-        CLASSIFICATION_CODE.PROJECTVERENIGING ||
-      this.model.classification?.get('id') ===
-        CLASSIFICATION_CODE.DIENSTVERLENENDE_VERENIGING ||
-      this.model.classification?.get('id') ===
-        CLASSIFICATION_CODE.OPDRACHTHOUDENDE_VERENIGING ||
-      this.model.classification?.get('id') ===
-        CLASSIFICATION_CODE.OPDRACHTHOUDENDE_VERENIGING_MET_PRIVATE_DEELNAME
-    );
-  }
-
-  get isPoliceZone() {
-    return (
-      this.model.classification?.get('id') === CLASSIFICATION_CODE.POLICE_ZONE
-    );
-  }
-
-  get isAssistanceZone() {
-    return (
-      this.model.classification?.get('id') ===
-      CLASSIFICATION_CODE.ASSISTANCE_ZONE
-    );
-  }
-
-  get isOcmwAssociation() {
-    return OCMW_ASSOCIATION_CLASSIFICATION_CODES.includes(
-      this.model.classification?.get('id')
-    );
-  }
-
+  // TODO: arguably this kind of logic belongs in the model
   get requiresGoverningBodies() {
     return !(
-      this.isAgbOrApb ||
-      this.isIgs ||
-      this.isPoliceZone ||
-      this.isAssistanceZone ||
-      this.isOcmwAssociation ||
+      this.model.isAgb ||
+      this.model.isApb ||
+      this.model.isIgs ||
+      this.model.isPoliceZone ||
+      this.model.isAssistanceZone ||
+      this.model.isOcmwAssociation ||
       this.model.isPevaMunicipality ||
       this.model.isPevaProvince ||
       this.model.isRepresentativeBody
@@ -95,10 +18,10 @@ export default class OrganizationsOrganizationController extends Controller {
 
   get requiresFunctionaries() {
     return !(
-      this.isDistrict ||
-      this.isWorshipOrganization ||
-      this.isPoliceZone ||
-      this.isAssistanceZone ||
+      this.model.isDistrict ||
+      this.model.isWorshipAdministrativeUnit ||
+      this.model.isPoliceZone ||
+      this.model.isAssistanceZone ||
       this.model.isPevaMunicipality ||
       this.model.isPevaProvince ||
       this.model.isRepresentativeBody
