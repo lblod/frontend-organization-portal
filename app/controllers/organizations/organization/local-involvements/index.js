32b7fb765ba16ad463fd3260a5339b25c7ef435b
mirdono <alexander.van.den.berghe@redpencil.io>
2024-04-19 17:02:54 +0200

    refactor: use equivalent model function instead of custom one in controller

--- app/controllers/organizations/organization/local-involvements/index.js
+++ app/controllers/organizations/organization/local-involvements/index.js
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
-import { CLASSIFICATION_CODE } from 'frontend-organization-portal/models/administrative-unit-classification-code';
 
 export default class OrganizationsOrganizationLocalInvolvementsIndexController extends Controller {
   queryParams = ['sort', 'page', 'size'];
@@ -10,12 +9,4 @@ export default class OrganizationsOrganizationLocalInvolvementsIndexController e
   @tracked sort = 'administrative-unit.name';
   @tracked page = 0;
   @tracked size = 20;
-
-  // TODO: replace by model function
-  get isWorshipService() {
-    return (
-      this.model.organization.classification?.get('id') ===
-      CLASSIFICATION_CODE.WORSHIP_SERVICE
-    );
-  }
 }
