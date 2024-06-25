6952e444aa3e6903da87c7e5bdc6a1cb6dba0cf7
mirdono <alexander.van.den.berghe@redpencil.io>
2024-06-25 12:11:32 +0200

    feat: related organizations view receives organisations as input
    The related organizations view table now receives the organisations that have a
    membership relation with the active organization. This avoids that the content
    of the rows depends on de `memberships` vs. `memberships-of-organizations`
    relations.
    
    A remaining issue is that the contents of the "Type relatie" containing the role
    label is not correctly set yet.

--- app/routes/organizations/organization/related-organizations/index.js
+++ app/routes/organizations/organization/related-organizations/index.js
@@ -13,17 +13,24 @@ export default class OrganizationsOrganizationRelatedOrganizationsIndexRoute ext
   async model(params) {
     const organization = this.modelFor('organizations.organization');
 
-    // TODO: consider active organizations filter
     const query = {
-      'filter[:or:][organization][:id:]': organization.id,
-      'filter[:or:][member][:id:]': organization.id,
-      include:
-        'member,member.classification,organization,organization.classification',
+      'filter[:or:][memberships][member][:id:]': organization.id,
+      'filter[:or:][memberships-of-organizations][organization][:id:]':
+        organization.id,
+      'filter[organization-status][:id:]': params.organizationStatus
+        ? '63cc561de9188d64ba5840a42ae8f0d6'
+        : undefined,
+      // include:
+      //   'member,member.classification,organization,organization.classification',
+      include: 'memberships,memberships-of-organizations',
       sort: params.sort,
       page: { size: params.size, number: params.page },
     };
 
-    const allMembershipRelations = await this.store.query('membership', query);
+    const allMembershipRelations = await this.store.query(
+      'organization',
+      query
+    );
 
     return {
       organization,
