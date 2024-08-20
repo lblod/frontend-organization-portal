087fb2a078017fc2901d34d6be96262f9e6f812c
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-08-20 11:51:09 +0200

    Update the deprecation workflow config
    This removes any handlers that are no longer needed, or which we need to resolve before updating to Ember v5.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,16 +1,5 @@
 /* eslint-disable no-undef */
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
-  workflow: [
-    { handler: 'silence', matchId: 'ember.built-in-components.import' }, // Ember inspector triggers this
-    { handler: 'silence', matchId: 'ember-global' }, // Ember-acmidm-login triggers this
-    { handler: 'silence', matchId: 'ember-modifier.use-destroyables' }, // PowerSelect triggers it
-    { handler: 'silence', matchId: 'ember-modifier.use-modify' }, // PowerSelect triggers it
-    {
-      handler: 'silence',
-      matchId: 'deprecated-run-loop-and-computed-dot-access',
-    }, // Ember-acmidm-login triggers this
-    { handler: 'silence', matchId: 'ember-modifier.no-args-property' }, // PowerSelect triggers it
-    { handler: 'silence', matchId: 'ember-modifier.no-element-property' }, // PowerSelect triggers it
-  ],
+  workflow: [],
 };
