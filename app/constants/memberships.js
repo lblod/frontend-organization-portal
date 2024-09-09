16e1f6034b3554d0136e66752e6446896a67a56d
mirdono <alexvdberghe@fastmail.com>
2024-09-09 11:40:36 +0200

    chore: improved documentation concerning allowed memberships

--- app/constants/memberships.js
+++ app/constants/memberships.js
@@ -1,10 +1,5 @@
 import { MEMBERSHIP_ROLES_MAPPING } from 'frontend-organization-portal/models/membership-role';
 import {
-  IGSCodeList,
-  OcmwAssociationCodeList,
-  PevaCodeList,
-} from 'frontend-organization-portal/constants/Classification';
-import {
   AgbCodeList,
   ApbCodeList,
   AssistanceZoneCodeList,
@@ -12,20 +7,33 @@ import {
   CentralWorshipServiceCodeList,
   CorporationOtherCodeList,
   DistrictCodeList,
+  IGSCodeList,
   MunicipalityCodeList,
+  OcmwAssociationCodeList,
   OCMWCodeList,
+  PevaCodeList,
   PevaMunicipalityCodeList,
   PevaProvinceCodeList,
   PoliceZoneCodeList,
   ProvinceCodeList,
   RepresentativeBodyCodeList,
   WorshipServiceCodeList,
-} from './Classification';
+} from 'frontend-organization-portal/constants/Classification';
 
-// Specifies which kinds of organisations can acts as participants in other
-// organisations. For each classification code in an organisations property the
-// classification codes for the allowed kinds of participants are listed in the
-// members property.
+// Specifies which organization classifications are allowed as participants in
+// other organization classifications. For each classification code in an
+// `organizations` property the classification codes for the allowed kinds of
+// participants are listed in the `members` property.
+//
+// For example,
+// ```
+// {
+//   organizations: [...AgbCodeList],
+//   members: [...MunicipalityCodeList],
+// }
+// ```
+// means "an AGB can have as participant a municipality" as well as the inverse
+// relation "a municipality can participate in an AGB".
 export const allowedParticipationMemberships = [
   {
     organizations: [...IGSCodeList],
@@ -66,6 +74,15 @@ export const allowedParticipationMemberships = [
   },
 ];
 
+// Same as above but for founding relationships between organizations.
+// ```
+// {
+//   organizations: [...AgbCodeList],
+//   members: [...MunicipalityCodeList],
+// },
+// ```
+// means "an AGB can have as founding organisation a municipality" and "a
+// municipality can found an AGB".
 export const allowedfoundingMemberships = [
   {
     organizations: [...AgbCodeList],
@@ -103,6 +120,16 @@ export const allowedfoundingMemberships = [
   },
 ];
 
+// Similar as above but for "has a relationship" memberships between
+// organizations. Note, from a user standpoint this kind of membership has no
+// direction. More concretely, for a user the following would be identical:
+// - "A has a relationship with B"; and
+// - "B has a relationship with A".
+//
+// We do enforce a specific direction, i.e. assignment of `organization` and
+// `member`, when storing memberships to ensure data consistency. This direction
+// is defined by the data structure below. The `getOppositeClassifications`
+// function takes care of presenting the user with the right options.
 export const allowedHasRelationWithMemberships = [
   {
     organizations: [...MunicipalityCodeList],
@@ -166,10 +193,10 @@ const allowedMembershipRelations = new Map([
  *     determine the appropriate classification codes.
  * @param {{@link OrganizationModel}} organization - The organization that is
  *     involved in the provided membership.
- * @returns {[string]} A list of classifications codes specifying the kinds of
+ * @returns {[string]} A list of classification codes specifying the kinds of
  *     organizations that are allowed to act as the other organization in the
  *     membership with the provided one. An empty list if the provided
- *     membership has no role or the the provided organization is not involved
+ *     membership has no role or if the provided organization is not involved
  *     in the provided membership.
  */
 export default function getOppositeClassifications(membership, organization) {
