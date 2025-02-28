be698a8ebccd48b145e6655b88baea7f3b8c5976
onodrim <alexander.van.den.berghe@redpencil.io>
2025-02-28 12:32:50 +0100

    refactor: replace non active relation modal with confirmation modal

---
+++ app/helpers/confirmation-message-text.js
@@ -0,0 +1,15 @@
+import { helper } from '@ember/component/helper';
+import { assert } from '@ember/debug';
+
+const CONFIRMATION_MESSAGE_TEXT = {
+  nonActiveRelatedOrganization:
+    'Opgelet, je hebt een niet actieve organisatie geselecteerd. Ben je zeker dat je deze relatie wenst toe te voegen?',
+};
+
+export default helper(function confirmationMessageText([key]) {
+  assert(
+    `{{confirmation-message-text}}: No confirmation message text value found for "${key}"`,
+    Boolean(CONFIRMATION_MESSAGE_TEXT[key]),
+  );
+  return CONFIRMATION_MESSAGE_TEXT[key];
+});
