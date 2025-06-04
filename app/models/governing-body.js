6136d36d37e62440f5da629c6e162d3ee98f7c8c
onodrim <alexander.van.den.berghe@redpencil.io>
2025-06-04 12:25:52 +0200

    chore: remove overlap helper functions that are no longer used
    These functions were only used in the, previously, removed
    `hasOverlapWithOtherGoverningBodies`. Since they are now no longer necessary
    they can be removed to avoid future confusion.

--- app/models/governing-body.js
+++ app/models/governing-body.js
@@ -5,7 +5,6 @@ import {
   validateBelongsToOptional,
   validateHasManyOptional,
 } from '../validators/schema';
-import { EXECUTIVE_ORGANEN } from './governing-body-classification-code';
 
 export default class GoverningBodyModel extends AbstractValidationModel {
   @attr('date') startDate;
@@ -104,67 +103,5 @@ export default class GoverningBodyModel extends AbstractValidationModel {
       mandates: validateHasManyOptional(),
       boardPositions: validateHasManyOptional(),
     });
-  }
-
-  /**
-   * Retrieve the identifier of the administrative unit this governing body is
-   * associated with, if any.
-   * @return {Promise<string>}
-   */
-  async #getAdministrativeUnitId() {
-    let untimedBody = await this.isTimeSpecializationOf;
-    if (untimedBody) {
-      let administrativeUnit = await untimedBody.administrativeUnit;
-      return administrativeUnit?.get('id');
-    }
-  }
-
-  /**
-   * Retrieve all untimed governing bodies for the administrative unit
-   * associated with this governing body.
-   * @return {Promise<A[GoverningBodyModel]>}
-   */
-  async #getUntimedGoverningBodies() {
-    let administrativeUnitId = await this.#getAdministrativeUnitId();
-
-    if (administrativeUnitId) {
-      let untimedGoverningBodies = await this.store.query('governing-body', {
-        filter: {
-          'administrative-unit': {
-            id: administrativeUnitId,
-          },
-        },
-        include: 'has-time-specializations,classification',
-      });
-      return untimedGoverningBodies;
-    }
-  }
-
-  /**
-   * Retrieve all other timed governing bodies that are associated with the same
-   * administrative unit as this governing body. This excludes any governing
-   * bodies that are time specialisations of governing bodies of that have and
-   * executive organ classification.
-   * @return {Promise<A[GoverningBodyModel]>}
-   */
-  async getOtherTimedGoverningBodies() {
-    let untimedGoverningBodies =
-      (await this.#getUntimedGoverningBodies())?.slice() ?? [];
-
-    let governingBodies = [];
-    for (const untimedGoverningBody of untimedGoverningBodies) {
-      let classification = await untimedGoverningBody.classification;
-      if (!EXECUTIVE_ORGANEN.includes(classification.id)) {
-        const timedGoverningBodies = untimedGoverningBody
-          ? await untimedGoverningBody.hasTimeSpecializations
-          : [];
-
-        const arrayTimedGoverningBodies = timedGoverningBodies.slice();
-
-        governingBodies.push(...arrayTimedGoverningBodies);
-      }
-    }
-
-    return governingBodies.filter((body) => body.id !== this.id);
   }
 }
