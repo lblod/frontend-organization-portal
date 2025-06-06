99ec3e6aa4a11c3be6a05345aa0bdbfd99fd4e2b
onodrim <alexander.van.den.berghe@redpencil.io>
2025-06-06 15:53:15 +0200

    feat: retrieve locations in scope and scope for locations
    Extend the scope service to cover the API of the corresponding backend
    service. This allows now to
    - retrieve all municipality-level locations within an organisation's scope of
    operation; and
    - retrieve a location resource that contains exactly a set of other location
    resources, to that can be used as scope of operation

--- app/services/scope-of-operation.js
+++ app/services/scope-of-operation.js
@@ -33,4 +33,73 @@ export default class ScopeOfOperationService extends Service {
 
     return label;
   }
+
+  /**
+   * Retrieve the location resources that are contained within the scope of
+   * operation of the provided organization. The location resources contained
+   * will be of the smallest level used, typically locations of municipality
+   * level.
+   * @param {Organization} organization - The organization for which the
+   *     retrieve its locations.
+   * @returns {Promise<Location[]>} An array containing the locations that are
+   *     within the provided organization's scope of operation.
+   */
+  async getLocationsInScope(organization) {
+    const scope = await organization.scope;
+
+    let locations;
+    if (organization.isAdministrativeUnit && scope) {
+      const resp = await fetch(
+        this.getScopeServiceEndpoint('locations-in-scope', scope),
+      );
+
+      if (resp.status === 200) {
+        const jsonResponse = await resp.json();
+        locations = await this.store.query('location', {
+          filter: {
+            ':id:': jsonResponse.join(','),
+          },
+          sort: 'label',
+        });
+      }
+    }
+    return locations;
+  }
+
+  /**
+   * Retrieve the location resource that exactly contains the provided
+   * locations. This location can be assigned to an organisation as its scope of
+   * operation.
+   * @param {...Location} locations - The locations that should be contained.
+   * @returns {Promise<Location | null>} - The location that exactly matches the
+   *     provided ones, null if no locations were provided.
+   */
+  async getScopeForLocations(...locations) {
+    if (locations.length === 0) {
+      return null;
+    }
+
+    const requestBody = {
+      data: {
+        locations: locations.map((location) => location.id),
+      },
+    };
+
+    const scopeUuid = await fetch(
+      this.getScopeServiceEndpoint('scope-for-locations'),
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/vnd.api+json',
+          'Content-Type': 'application/vnd.api+json',
+        },
+        body: JSON.stringify(requestBody),
+      },
+    );
+
+    const uuid = await scopeUuid.json();
+    const scopeResource = await this.store.findRecord('location', uuid);
+
+    return scopeResource;
+  }
 }
