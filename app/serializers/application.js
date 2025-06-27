367201f83a8f7be02987c7f4b8d364a8639c021f
onodrim <alexander.van.den.berghe@redpencil.io>
2025-06-27 10:36:02 +0200

    chore: filter out URI keys from POST calls

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -4,4 +4,9 @@ import DataTableSerializerMixin from 'ember-data-table/mixins/serializer';
 
 export default class ApplicationSerializer extends JSONAPISerializer.extend(
   DataTableSerializerMixin,
-) {}
+) {
+  serializeAttribute(snapshot, json, key, attributes) {
+    if (key !== 'uri')
+      super.serializeAttribute(snapshot, json, key, attributes);
+  }
+}
