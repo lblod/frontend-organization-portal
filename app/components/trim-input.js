d3c3714e22c8831b2d84a40115dff0d40f629b20
wolfez <wolfderechter@gmail.com>
2025-08-13 11:19:13 +0200

    trim value to avoid false positives

--- app/components/trim-input.js
+++ app/components/trim-input.js
@@ -4,7 +4,7 @@ import { restartableTask } from 'ember-concurrency';
 export default class TrimInputComponent extends Component {
   @restartableTask
   *trimInput(event) {
-    const input = yield event.target.value;
-    if (this.args.value !== input) this.args.onUpdate(input.trim());
+    const input = (yield event.target.value).trim();
+    if (this.args.value !== input) this.args.onUpdate(input);
   }
 }
