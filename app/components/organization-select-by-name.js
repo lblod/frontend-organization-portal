64a3a2cac11e5b53ffd80c75fe20553b987ec6d4
Damon <damonkennes@gmail.com>
2025-11-20 15:48:28 +0100

    OP-3623: edited organization-select-by-identifier/name to use already selected filters on their search results

--- app/components/organization-select-by-name.js
+++ app/components/organization-select-by-name.js
@@ -1,7 +1,10 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { restartableTask } from 'ember-concurrency';
-import { selectByRole as getClassificationIds } from 'frontend-organization-portal/utils/classification-identifiers';
+import {
+  selectByRole as getClassificationIds,
+  getClassificationIdsForRole,
+} from 'frontend-organization-portal/utils/classification-identifiers';
 
 export default class OrganizationSelectByNameComponent extends Component {
   @service muSearch;
@@ -15,9 +18,62 @@ export default class OrganizationSelectByNameComponent extends Component {
       filter[`:phrase_prefix:legal_name,alternative_name,name`] = searchParams;
     }
 
-    filter['classification_id'] = getClassificationIds(
-      this.currentSession.hasWorshipRole,
-    );
+    let queryClassifications = [];
+    if (
+      this.args.selectedClassificationIds &&
+      this.args.selectedOrganizationTypes
+    ) {
+      const idsFromOrganizationType = getClassificationIdsForRole(
+        this.currentSession.hasWorshipRole,
+        false,
+        ...this.args.selectedOrganizationTypes.split(','),
+      );
+
+      const idsFromClassifications =
+        this.args.selectedClassificationIds.split(',');
+
+      queryClassifications = idsFromOrganizationType
+        .filter((id) => idsFromClassifications.includes(id))
+        .map((id) => `(classification_id:${id})`)
+        .join(' OR ');
+    } else if (this.args.selectedOrganizationTypes) {
+      queryClassifications = getClassificationIdsForRole(
+        this.currentSession.hasWorshipRole,
+        false,
+        ...this.args.selectedOrganizationTypes.split(','),
+      ).join(' OR ');
+    } else if (this.args.selectedClassificationIds) {
+      queryClassifications = this.args.selectedClassificationIds
+        .split(',')
+        .map((id) => `(classification_id:${id})`)
+        .join(' OR ');
+    } else {
+      queryClassifications = getClassificationIds(
+        this.currentSession.hasWorshipRole,
+      );
+    }
+    filter[':query:classification_id'] = queryClassifications;
+
+    if (this.args.selectedMunicipality) {
+      filter[':phrase:municipality'] = this.args.selectedMunicipality;
+    }
+
+    if (this.args.selectedProvince) {
+      filter[':phrase:province'] = this.args.selectedProvince;
+    }
+
+    if (this.args.selectedRecognizedWorshipTypeId) {
+      filter['recognized_worship_type_id'] =
+        this.args.selectedRecognizedWorshipTypeId;
+    }
+
+    if (this.args.selectedOrganizationStatus) {
+      filter['status_id'] = this.args.selectedOrganizationStatus;
+    }
+
+    if (this.args.selectedIdentifier) {
+      filter[':prefix:identifier.index'] = this.args.selectedIdentifier;
+    }
 
     const result = yield this.muSearch.search({
       index: 'organizations',
