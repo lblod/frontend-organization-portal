cd43afe10f7db32f0b4810277346bc5791034caf
Damon <damonkennes@gmail.com>
2026-02-10 14:26:17 +0100

    OP-3726: make trimInput work on enter keypress

--- app/components/trim-input.js
+++ app/components/trim-input.js
@@ -1,4 +1,5 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 import { restartableTask } from 'ember-concurrency';
 
 export default class TrimInputComponent extends Component {
@@ -6,5 +7,12 @@ export default class TrimInputComponent extends Component {
   *trimInput(event) {
     const input = (yield event.target.value).trim();
     if (this.args.value !== input) this.args.onUpdate(input);
+  }
+
+  @action
+  handleKeydown(event) {
+    if (event.key === 'Enter') {
+      this.trimInput.perform(event);
+    }
   }
 }
