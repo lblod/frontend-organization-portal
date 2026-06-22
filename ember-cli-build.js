999e9e9b8e8d57a6ed31051bd8c12010cd1d6a80
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-06-22 18:02:43 +0200

    Switch to the Typescript blueprint
    This allows us to add new .ts code in the future. The setup is a bit outdated, but we'll get updates by updating the blueprint in the future, and the old setup still works ok-ish.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
+    'ember-cli-babel': { enableTypeScriptTransform: true },
     'ember-simple-auth': {
       useSessionSetupMethod: true,
     },
