296ba9903590bf55994d0edf724882c45b58e3bd
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-06-24 10:52:25 +0200

    Always display the "Andere vestiging" option

--- app/components/site-type-select.js
+++ app/components/site-type-select.js
@@ -33,26 +33,6 @@ export default class SiteTypeSelectComponent extends Component {
       filteredTypes.push(
         allTypes.find((type) => type.id == '57e8e5498ca84056b8a87631a26c90af'), // Gemeentehuis
       );
-      filteredTypes.push(
-        allTypes.find(
-          (type) => type.id == 'dcc01338-842c-4fbd-ba68-3ca6f3af975c',
-        ), // Andere vestiging
-      );
-    } else if (
-      this.args.organization.isAgb ||
-      this.args.organization.isApb ||
-      this.args.organization.isIGS ||
-      this.args.organization.isOcmwAssociation ||
-      this.args.organization.isPevaMunicipality ||
-      this.args.organization.isPevaProvince ||
-      this.args.organization.isAssociationOther ||
-      this.args.organization.isCorporationOther
-    ) {
-      filteredTypes.push(
-        allTypes.find(
-          (type) => type.id == 'dcc01338-842c-4fbd-ba68-3ca6f3af975c',
-        ), // Andere vestiging
-      );
     } else if (this.args.organization.isProvince) {
       filteredTypes.push(
         allTypes.find((type) => type.id == '15f2683c61b74541b27b64b4365806c7'), // Provinciehuis
@@ -68,6 +48,12 @@ export default class SiteTypeSelectComponent extends Component {
         ), // Hoofdcommissariaat
       );
     }
+
+    filteredTypes.push(
+      allTypes.find(
+        (type) => type.id == 'dcc01338-842c-4fbd-ba68-3ca6f3af975c', // Andere vestiging
+      ),
+    );
 
     return filteredTypes;
   }
