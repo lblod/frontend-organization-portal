43be45a6f090b6f7bcb42c3f8761c1c36799d1bb
Damon <damonkennes@gmail.com>
2026-06-24 16:00:14 +0200

    remove concept-schema model

--- app/models/concept.js
+++ app/models/concept.js
@@ -1,14 +1,8 @@
-import Model, { attr, belongsTo } from '@ember-data/model';
+import Model, { attr } from '@ember-data/model';
 
 export default class ConceptsModel extends Model {
   @attr label;
   @attr altLabel;
   @attr notation;
-  @attr('number') order;
-
-  @belongsTo('concept-scheme', {
-    inverse: null,
-    async: true,
-  })
-  inScheme;
+  @attr order;
 }
