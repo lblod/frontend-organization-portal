422aade8f91cd179c094c7d6657a446b946a4889
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-07-28 14:37:16 +0200

    Update to ember-concurrency v5 and power-select v8

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
+    babel: {
+      plugins: [
+        require.resolve('ember-concurrency/async-arrow-task-transform'),
+      ],
+    },
     'ember-simple-auth': {
       useSessionSetupMethod: true,
     },
