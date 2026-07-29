5a16bdad90a6148b5b32532459361af31c8f0739
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-07-29 17:02:43 +0200

    fixup 6.7

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,3 @@
-import './deprecation-workflow';
 import Application from '@ember/application';
 import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 import Resolver from 'ember-resolver';
@@ -7,10 +6,9 @@ import config from 'frontend-organization-portal/config/environment';
 import './config/custom-inflector-rules';
 import { silenceEmptySyncRelationshipWarnings } from './utils/ember-data';
 
-silenceEmptySyncRelationshipWarnings();
-
 if (macroCondition(isDevelopingApp())) {
   importSync('./deprecation-workflow');
+  silenceEmptySyncRelationshipWarnings();
 }
 
 export default class App extends Application {
