a37f4c43dfe7b37328021289e218d13c0406bfbf
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-07-30 17:44:05 +0200

    Replace `store.findRecord` with the `store.request` equivalent

--- app/routes/people/person/positions/functionary.js
+++ app/routes/people/person/positions/functionary.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { findRecord } from '@warp-drive/legacy/compat/builders';
 
 export default class PeoplePersonPositionsFunctionaryRoute extends Route {
   @service store;
@@ -7,16 +8,14 @@ export default class PeoplePersonPositionsFunctionaryRoute extends Route {
   async model({ functionaryId }) {
     let person = this.modelFor('people.person');
 
-    let functionary = await this.store.findRecord(
-      'functionary',
-      functionaryId,
-      {
+    const { content: functionary } = await this.store.request(
+      findRecord('functionary', functionaryId, {
         reload: true,
         include: [
           'board-position.governing-bodies.is-time-specialization-of.classification',
           'board-position.governing-bodies.is-time-specialization-of.administrative-unit',
         ].join(),
-      },
+      }),
     );
 
     return {
